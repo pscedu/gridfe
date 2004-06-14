2f2a8a9753fe855cb84876fa92bcf20b17dda5e3
yanovich <>
2004-06-14 11:14:50 +0000

    add (un)escapeHTML

--- src/gridfe/JASP.java
+++ src/gridfe/JASP.java
@@ -1,4 +1,51 @@
 /* $Id$ */
 public class JASP {
-	
+	private entity[] entmap = {
+		{ '"', "quot" },
+//		{ '\'', "apos" },
+		{ '&', "amp" },
+		{ '<', "lt" },
+		{ '>', "gt" }
+	};
+	private String validHTML =
+		"abcdefghijklmnopqrstuvwxyz" +
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+		"0123456789,./?;:[]{}\\|`~!@#$%^*()_+-=";
+		
+	public String escapeHTML(String s) {
+		int i, j;
+		String t = s.copy();
+		for (i = 0; i < t.length(); t++) {
+			/* Entity names. */
+			for (j = 0; j < entmap.length(); j++)
+				if (entmap[j].raw == s[i]) {
+					t += entmap[j].esc;
+					continue;
+				}
+			/*
+			 * Characters that do not need to be
+			 * represented by entities.
+			 */
+			for (j = 0; j < validHTML.length(); j++)
+				if (validHTML[j] == s[i]) {
+					t += s[i]
+					continue;
+				}
+			/*
+			 * Character not allowed; use ASCII
+			 * value.
+			 */
+			t += "&#" + s[i].ord() + ";";
+		}
+	}
+
+	public String unescapeHTML(String s) {
+		String t = s.copy();
+		return t;
+	}
+};
+
+class entity {
+	public char raw;
+	public String esc;
 };

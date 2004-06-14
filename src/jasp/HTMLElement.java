39ac548c46d2733f26dcda9fcddc8b3f0fb56b2e
yanovich <>
2004-06-14 16:16:16 +0000

    flesh out

--- src/jasp/HTMLElement.java
+++ src/jasp/HTMLElement.java
@@ -1,36 +1,64 @@
 /* $Id$ */
+package jasp;
+
+import java.util.*;
+
 public class HTMLElement {
+	private JASP jasp;
 	private String name;
 	private String value;
-	private Attribute[] attrs;
-	private Style[] styles;
+	private List[] attrs;
+	private List[] styles;
 
-	public build() {
+	HTMLElement(JASP jasp, String name, String value) {
+		this.jasp  = jasp;
+		this.name  = name;
+		this.value = value;
+	}
+
+	HTMLElement(JASP jasp, String name) {
+		this.jasp = jasp;
+		this.name = name;
+	}
+
+	public String build() {
 		String s;
+		Attribute attr;
+		Iterator i;
 
 		s = "<" + this.name;
 
-		for (i = 0; i < attrs.length(); i++)
-			s += " " + attrs[i].name + "=\""
-			  + oof.jasp.escapeHTML(attr.value) + "\"";
-
-		s += value ? 
-
-		if (value) {
-		} else {
+		if (!this.styles.isEmpty()) {
+			String t = "";
+			for (i = this.styles.iterator(); i.hasNext(); ) {
+				attr = (Attribute)i.next();
+				t += attr.name + ":" + attr.value + ";";
+			}
+			this.addAttribute("style", t);
 		}
 
-		s += "";
+		for (i = this.attrs.iterator(); i.hasNext(); ) {
+			attr = (Attribute)i.next();
+			s += " " + attr.name + "=\""
+			  + this.jasp.escapeHTML(attr.value) + "\"";
+		}
+
+		if (this.value == null)
+			this.value = "";
+		if (this.value == "" || this.name == "div")
+			s += ">" + this.value + "</" + this.name+ ">";
+		else
+			s += " />";
+
+		return s;
 	}
 
 	public void addAttribute(String name, String value) {
-		attrs[attrs.length++] = ;
+		this.attrs.add(new Attribute(name, value));
 	}
 
 	public void addStyle(String name, String value) {
-		if (styles == null) {
-			
-		}
+		this.styles.add(new Attribute(name, value));
 	}
 
 	public void setValue(String s) {
@@ -40,4 +68,9 @@ public class HTMLElement {
 	public void appendValue(String s) {
 		this.value += s;
 	}
+}
+
+class Attribute {
+	public String name;
+	public String value;
 }

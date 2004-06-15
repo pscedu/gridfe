3610f7f3ccc8c44e8b0bf38b18435dfe2bc14f11
yanovich <>
2004-06-15 14:27:31 +0000

    rename BASE to ELEMENT; remove worthless method

--- src/oof/filter/XHTML.java
+++ src/oof/filter/XHTML.java
@@ -6,16 +6,12 @@ import oof.element.*;
 public class XHTML extends Filter {
 	private OOF oof;
 
-	public build(BASE e) {
+	public build(ELEMENT e) {
 		HTMLElement html = new HTMLElement(e.name);
 		String s;
 		Attribute attr;
 
 
 		return html.build();
-	}
-	
-	public build(Paragraph p) {
-		return this.build(p);
 	}
 }

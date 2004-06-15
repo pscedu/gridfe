4d943b63dfca8b19157138a449de3844d1165fd6
yanovich <>
2004-06-15 14:26:33 +0000

    spc; add jasp

--- src/gridfe/Page.java
+++ src/gridfe/Page.java
@@ -1,9 +1,12 @@
 /* $Id$ */
 public class Page {
-	private OOF oof;
+	private OOF  oof;
+	private JASP jasp;
 
 	Page() {
-		this.oof = new OOF();
+		/* XXX: load jasp prefs from resource. */
+		this.jasp = new JASP();
+		this.oof  = new OOF(this.jasp, "xhtml");
 	}
 
 	public void header(String title) {
@@ -12,12 +15,12 @@ public class Page {
 		s = "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\">"
 		  + "<html lang=\"en-US\" xml:lang=\"en-US\" xmlns=\"http://www.w3.org/1999/xhtml\">"
 		  + 	"<head>"
-		  +		 	"<title>" + this.escapeHTML(title) + "</title>"
-		  +			"<meta http-equiv=\"Content-Type\" content=\"text/html; charset=iso-8859-1\" />"
-		  +			"<link rel=\"stylesheet\" type=\"text/css\" href=\"/lib/main.css\" media=\"screen\">"
+		  +	 	"<title>" + this.jasp.escapeHTML(title) + "</title>"
+		  +		"<meta http-equiv=\"Content-Type\" content=\"text/html; charset=iso-8859-1\" />"
+		  +		"<link rel=\"stylesheet\" type=\"text/css\" href=\"/lib/main.css\" media=\"screen\">"
 		  + 	"</head>"
 		  + 	"<body>"
-		  +			"";
+		  +		"";
 
 		return s;
 	}
@@ -28,10 +31,6 @@ public class Page {
 		s =		"</body>"
 		  + "</html>";
 
-		return s;
-	}
-
-	public String escapeHTML(String s) {
 		return s;
 	}
 };

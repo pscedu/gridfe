39cfaa12bd6566e46efa61e1f30a50237cd55077
yanovich <>
2004-06-15 15:53:54 +0000

    fix entmap construction; fix string copying; public constructors; decl pkg

--- src/jasp/JASP.java
+++ src/jasp/JASP.java
@@ -1,27 +1,29 @@
 /* $Id$ */
+package jasp;
+
 public class JASP {
-	private entity[] entmap = {
-		{ '"', "quot" },
-//		{ '\'', "apos" },
-		{ '&', "amp" },
-		{ '<', "lt" },
-		{ '>', "gt" }
+	private entity[] entmap = new entity[] {
+		new entity('"', "quot"),
+//		new entity('\'', "apos"),
+		new entity('&', "amp"),
+		new entity('<', "lt"),
+		new entity('>', "gt"),
 	};
 	private String validHTML =
 		"abcdefghijklmnopqrstuvwxyz" +
 		"ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
 		"0123456789,./?;:[]{}\\|`~!@#$%^*()_+-=";
 	
-	JASP() {
+	public JASP() {
 	}
 		
 	public String escapeHTML(String s) {
 		int i, j;
-		String t = s.copy();
-		for (i = 0; i < t.length(); t++) {
+		String t = new String(s);
+		for (i = 0; i < t.length(); i++) {
 			/* Entity names. */
-			for (j = 0; j < entmap.length(); j++)
-				if (entmap[j].raw == s[i]) {
+			for (j = 0; j < entmap.length; j++)
+				if (entmap[j].raw == s.charAt(i)) {
 					t += entmap[j].esc;
 					continue;
 				}
@@ -30,20 +32,21 @@ public class JASP {
 			 * represented by entities.
 			 */
 			for (j = 0; j < validHTML.length(); j++)
-				if (validHTML[j] == s[i]) {
-					t += s[i]
+				if (validHTML.charAt(j) == s.charAt(i)) {
+					t += s.charAt(i);
 					continue;
 				}
 			/*
 			 * Character not allowed; use ASCII
 			 * value.
 			 */
-			t += "&#" + s[i].ord() + ";";
+			t += "&#" + ((int)s.charAt(i)) + ";";
 		}
+		return t;
 	}
 
 	public String unescapeHTML(String s) {
-		String t = s.copy();
+		String t = new String(s);
 		return t;
 	}
 };
@@ -51,4 +54,9 @@ public class JASP {
 class entity {
 	public char raw;
 	public String esc;
+
+	public entity(char raw, String esc) {
+		this.raw = raw;
+		this.esc = esc;
+	}
 };

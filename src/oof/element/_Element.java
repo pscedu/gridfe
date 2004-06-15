3b553f88467e5cff4d95b58f320875241630c310
yanovich <>
2004-06-15 14:27:46 +0000

    revamp

--- src/oof/element/_Element.java
+++ src/oof/element/_Element.java
@@ -2,73 +2,44 @@
 package oof.element;
 
 public class ELEMENT {
-	private Container[] children;
-	private int len, pos;
 	private String name;
-	private Attribute[] attrs;
+	private LinkedList attrs;
+	private LinkedList children;
 
-	Element() {
-		this.len = this.pos = 0;
+	ELEMENT(Object[] attrs = {}, String s = null) {
+		this.attrs    = new LinkedList();
+		this.children = new LinkedList(s);
 	}
 
-	Element(String s) {
-		this.len = this.pos = 0;
-		this.append(s);
+	ELEMENT(Object[] attrs = {}, ELEMENT e = null) {
+		this.attrs    = new LinkedList();
+		this.children = new LinkedList(e);
 	}
 
-	Element(Element e) {
-		this.len = this.pos = 0;
-		this.append(e);
-	}
-
-	private void ensure(int newlen) {
-		Container[] newchildren;
-		newchildren = new Container[newlen];
-		if (this.len)
-			System.copyarray(this.children, newchildren, 0, 0, this.len);
-		this.children = newchildren;
-		this.len = newlen;
+	ELEMENT() {
 	}
 
 	public void append(String s) {
-		this.ensure(++this.pos);
-		children[this.pos].type = TSTRING;
-		children[this.pos].s = s;
+		this.children.addLast(s);
 	}
 
-	public void append(Element e) {
-		this.ensure(++this.pos);
-		children[this.pos].type = TELEM;
-		children[this.pos].e = e;
+	public void append(ELEMENT e) {
+		this.children.addLast(e);
 	}
 
-/*
 	public void prepend(String s) {
-		this.ensure(++this.pos);
-		for (int i = 0; i < this.len)
-			
-		children[this.pos].type = TSTRING;
-		children[this.pos].s = s;
+		this.children.addFirst(s);
 	}
 
 	public void prepend(Element e) {
-		this.ensure(++this.pos);
-		children[this.pos].type = TELEM;
-		children[this.pos].e = e;
+		this.children.addFirst(e);
 	}
-*/
 
 	public String toString() {
 		throw new OOFToStringException();
 	}
 
 	public String build() {
-		return 
+		return this.filter.build(this);
 	}
-};
-
-protected class Container {
-	public int type;
-	public String s;
-	public Element e;
 };

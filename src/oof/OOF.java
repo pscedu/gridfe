875e7ce6ff754b198ac4163c217b1f701d4b5a30
yanovich <>
2004-06-15 15:51:49 +0000

    import jasp; public constructors

--- src/oof/OOF.java
+++ src/oof/OOF.java
@@ -1,13 +1,14 @@
 /* $Id$ */
 package oof;
 
+import jasp.*;
 import oof.filter.*;
 
 public class OOF {
 	private Filter filter;
 	private JASP jasp;
 
-	OOF(JASP j, String filter) {
+	public OOF(JASP j, String filter) {
 		this.jasp = j;
 	}
 };

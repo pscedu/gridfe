d325b87ed25b9fb3f9928e3ed9f8db51eab9a556
yanovich <>
2004-06-16 18:01:52 +0000

    add constructor

--- src/oof/filter/Filter.java
+++ src/oof/filter/Filter.java
@@ -4,5 +4,11 @@ package oof.filter;
 import oof.element.*;
 
 public abstract class FILTER {
+	private OOF oof;
+
+	public FILTER(OOF oof) {
+		this.oof = oof;
+	}
+
 	public abstract String build(ELEMENT e);
 };

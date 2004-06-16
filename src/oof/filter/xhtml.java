a6085f3f125ae294d6674a0b976b9a498d587314
yanovich <>
2004-06-16 20:09:44 +0000

    rename; add constructor

--- src/oof/filter/xhtml.java
+++ src/oof/filter/xhtml.java
@@ -3,7 +3,11 @@ package oof.filter;
 
 import oof.element.*;
 
-public class XHTML extends FILTER {
+public class xhtml extends FILTER {
+	public xhtml(OOF oof) {
+		super(oof);
+	}
+
 	public String build(ELEMENT e) {
 		HTMLElement html = new HTMLElement(e.name);
 		String s;

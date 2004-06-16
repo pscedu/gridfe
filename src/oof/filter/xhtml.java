2b34af7116da245b1639947d462dc3a2ca5094a9
yanovich <>
2004-06-16 20:22:33 +0000

    remove junk; fix constructor

--- src/oof/filter/xhtml.java
+++ src/oof/filter/xhtml.java
@@ -1,19 +1,17 @@
 /* $Id$ */
 package oof.filter;
 
+import jasp.*;
+import oof.*;
 import oof.element.*;
 
 public class xhtml extends FILTER {
-	public xhtml(OOF oof) {
-		super(oof);
+	public xhtml(JASP jasp, OOF oof) {
+		super(jasp, oof);
 	}
 
 	public String build(ELEMENT e) {
-		HTMLElement html = new HTMLElement(e.name);
-		String s;
-		Attribute attr;
-
-
+		HTMLElement html = new HTMLElement(this.jasp, e.name);
 		return html.build();
 	}
 }

93e218983658c6522f50f2604af4c90098cd31f6
yanovich <>
2004-06-18 13:04:42 +0000

    add abstract element methods

--- src/oof/filter/Filter.java
+++ src/oof/filter/Filter.java
@@ -14,5 +14,32 @@ public abstract class FILTER {
 		this.oof  = oof;
 	}
 
-	public abstract String build(ELEMENT e);
+	public abstract String build(Break e);
+	public abstract String build(Code e);
+	public abstract String build(DivisionEnd e);
+	public abstract String build(Division e);
+	public abstract String build(DivisionStart e);
+	public abstract String build(Email e);
+	public abstract String build(Emphasis e);
+	public abstract String build(Fieldset e);
+	public abstract String build(FormEnd e);
+	public abstract String build(Form e);
+	public abstract String build(FormStart e);
+	public abstract String build(Header e);
+	public abstract String build(HorizontalRuler e);
+	public abstract String build(Image e);
+	public abstract String build(Input e);
+	public abstract String build(Link e);
+	public abstract String build(ListEnd e);
+	public abstract String build(ListItem e);
+	public abstract String build(List e);
+	public abstract String build(ListStart e);
+	public abstract String build(Paragraph e);
+	public abstract String build(Preformatted e);
+	public abstract String build(Span e);
+	public abstract String build(Strong e);
+	public abstract String build(TableEnd e);
+	public abstract String build(Table e);
+	public abstract String build(TableRow e);
+	public abstract String build(TableStart e);
 };

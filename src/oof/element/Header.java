bb6a4366ed713d01c14d9d1acf3ec63289eadd88
yanovich <>
2004-06-21 17:25:55 +0000

    java sucks, and fix header

--- src/oof/element/Header.java
+++ src/oof/element/Header.java
@@ -5,8 +5,11 @@ import oof.*;
 import oof.element.*;
 
 public class Header extends ELEMENT {
-	public Header(OOF oof, Object[] attrs, Object[] os) {
+	public String size;
+	public Header(OOF oof, Object[] attrs, Object[] os) throws OOFBadElementFormException {
 		super(oof, attrs, os);
+		if ((this.size = this.removeAttribute("size")) == null)
+			throw new OOFBadElementFormException("header");
 	}
 
 	public String toString() {

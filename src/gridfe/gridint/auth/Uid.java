4ffe0e93701eaec9f9423a5aa3a5663fd97c3536
rbudden <>
2004-06-24 15:08:52 +0000

    Initial Checkin - Wrapper for user id

---
+++ src/gridfe/gridint/auth/Uid.java
@@ -0,0 +1,31 @@
+/*
+** Data type wrapper over the uid
+*/
+package gridint.auth;
+
+public class Uid
+{
+	private int uid;
+
+	public Uid(int uid)
+	{
+		this.uid = uid;
+	}
+
+	public Uid(Integer uid)
+	{
+		this.uid = uid.intValue();
+	}
+	
+	public Uid(String uid)
+	{
+		Integer i = new Integer(uid);
+		this.uid = i.intValue();
+	}
+
+	public int intValue()
+	{
+		return this.uid;
+	}
+}
+

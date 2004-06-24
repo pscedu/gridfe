e49296bace19b8c68ae43a2bed9eebc81977ce37
rbudden <>
2004-06-24 19:55:29 +0000

    First testable version

--- src/gridfe/gridint/GramInt.java
+++ src/gridfe/gridint/GramInt.java
@@ -17,30 +17,66 @@ public class GramInt
 
 	public GramInt(GSSCredential gss)
 	{
-
+		this.gss = gss;
+	}
+	public GramInt(GSSCredential gss, String host)
+	{
+		this.gss = gss;
+		this.host = host;
 	}
 
+	/*
+	** globus-job-submit & globus-job-run
+	*/
 	public void gramJobSubmit(String host, String rsl) throws GramException, GSSException
 	{
-		this.host = host;
-		this.rsl = rsl;
 		this.batch = true;
-		this.gramRequest();
+		this.gramRequest(host, rsl);
 	}
 
 	public void gramJobRun(String host, String rsl) throws GramException, GSSException
 	{
+		this.batch = false;
+		this.gramRequest(host, rsl);
+	}
+
+	private void gramRequest(String host, String rsl) throws GramException, GSSException
+	{
 		this.host = host;
 		this.rsl = rsl;
-		this.batch = false;
-		this.gramRequest();
-	}
 
-	private void gramRequest() throws GramException, GSSException
-	{
+		/* Make sure the host is there */
 		Gram.ping(this.host);
 
+		/* Create and Process Job */
 		this.job = new GramJob(this.gss, this.rsl);
+		this.job.request(this.host, this.batch);
+
+		/*
+		** Add callback listener for status change
+		** which requires "implements GramJobListener"
+		** and a public method call "stateChanged"
+		*/
+		//this.job.addListener(this);
 		
 	}
+
+	/* globus-job-status */
+	public int getStatus()
+	{
+		return this.job.getStatus();
+	}
+
+	/* globus-job-cancel */
+	public void cancel() throws GramException, GSSException
+	{
+		this.job.cancel();
+	}
 }
+
+
+
+
+
+
+

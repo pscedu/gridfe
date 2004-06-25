3901bddb32dbf8bb4aa77f167146e9f0675d11f5
rbudden <>
2004-06-25 20:34:17 +0000

    Initial Checkin

---
+++ src/gridfe/gridint/RSLElement.java
@@ -0,0 +1,111 @@
+/*
+** RSL (Resource Specification Language) Element
+*/
+package gridint;
+
+public class RSLElement
+{
+	/* Strings in the form "&(executable=`which hostname`)..." */
+	private final String b = "(";
+	private final String m = "=";
+	private final String e = ")";
+	private final String s = " ";
+	private final String q = "\"";
+
+	/* 
+	** Prepend Args
+	** Specification: (Multi+, Conjunct&, Disjunct|)
+	*/
+	private String pre = "&";
+
+	private StringBuffer data;
+	private String[] gParam, gValue, vValue, kValue, kKey;
+	private String vParam, kParam;
+
+
+	public RSLElement()
+	{
+	}
+	public RSLElement(String[] param, String[] value)
+	{
+		this.setGenerics(param, value);
+	}
+
+	public void changeSpec(String s)
+	{
+		this.pre = new String(s);
+	}
+
+	public void setGenerics(String[] param, String[] value)
+	{
+		this.gParam = (String[])param.clone();
+		this.gValue = (String[])value.clone();
+	}
+	public void setVarArgs(String param, String[] value)
+	{
+		this.vParam = new String(param);
+		this.vValue = (String[])value.clone();
+	}
+	public void setKeyPairs(String param, String[] key, String[] value)
+	{
+		this.kParam = new String(param);
+		this.kKey = (String[])key.clone();
+		this.kValue = (String[])value.clone();
+	}
+
+	/* Generic build for "(param=value)" */
+	public void buildGenerics(String[] param, String[] value)
+	{
+		for(int i = 0; i < param.length; i++)
+			this.data.append(b+param[i]+m+value[i]+e);
+	}
+
+	/* 
+	** Build in the form of '(param="arg1" "arg2")' 
+	** Example: (arguments="arg1" "arg number 2");
+	*/
+	public void buildVarArgs(String param, String[] value)
+	{
+		this.data.append(b+param+m);
+
+		/* Quote all args to be safe! */
+		for(int i = 0; i < value.length; i++)
+			this.data.append(q+value[i]+q+s);
+
+		this.data.append(e);
+	}
+
+	/* 
+	** Build in the form of "(param=(key1 value1)(key2 value2))" 
+	** Example: (environment=(MANPATH /usr/man)(EDITOR vi));
+	*/
+	public void buildKeyPairs(String param, String[] key, String[] value)
+	{
+		this.data.append(b+param+m);
+
+		/* Quote all args to be safe! */
+		for(int i = 0; i < key.length; i++)
+			this.data.append(b+q+key[i]+q+s+q+value[i]+q+e);
+
+		this.data.append(e);
+	}
+
+	private void build()
+	{
+		this.data = new StringBuffer(pre);
+
+		/* Build those that are not empty */
+		if(this.gParam != null)
+			this.buildGenerics(this.gParam, this.gValue);
+		if(this.vParam != null)
+			this.buildVarArgs(this.vParam, this.vValue);
+		if(this.kParam != null)
+			this.buildKeyPairs(this.kParam, this.kKey, this.kValue);
+	}
+
+	public String toString()
+	{
+		this.build();
+		return this.data.toString();
+	}
+}

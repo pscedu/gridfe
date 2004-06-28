4f24087d1c85927ff11779409545971452fdf3f4
rbudden <>
2004-06-28 19:34:30 +0000

    $COG_INSTALL_PATH

--- lib/mk/defs.mk
+++ lib/mk/defs.mk
@@ -3,4 +3,4 @@
 MAINMK = ${SYSROOT}/lib/mk/main.mk
 JAVAC = `which javac`
 JAVA = `which java`
-JFLAGS = -g -classpath ${SYSROOT}/lib:/usr/share/java/servlet.jar:/usr/java/cog-1.2/lib/cog-jglobus.jar:/usr/java/j2sdk1.4.2_04/jre/lib/rt.jar:/usr/java/cog-1.2/lib/puretls.jar:/usr/java/cog-1.2/lib/cryptix32.jar:/usr/java/cog-1.2/lib/cryptix-asn1.jar:/home/rbudden/code/gridfe/lib:.
+JFLAGS = -g -classpath ${SYSROOT}/lib:/usr/share/java/servlet.jar:${COG_INSTALL_PATH}/lib/cog-jglobus.jar:/usr/java/j2sdk1.4.2_04/jre/lib/rt.jar:${COG_INSTALL_PATH}/lib/puretls.jar:${COG_INSTALL_PATH}/lib/cryptix32.jar:${COG_INSTALL_PATH}/lib/cryptix-asn1.jar:.

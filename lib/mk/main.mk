f9100a0e1216e3d8244c89438a33093be04d4967
yanovich <>
2004-06-28 19:02:33 +0000

    split recursion, will hopefully be readdressed later; add java compiling

--- lib/mk/main.mk
+++ lib/mk/main.mk
@@ -1,14 +1,35 @@
 # $Id$
 
-all clean depend: ${TARGET}
-	@for i in ${SUBDIRS}; do					\
-		echo -n "===> ";					\
-		if [ -n "${DIRPREFIX}" ]; then				\
-			echo -n ${DIRPREFIX};				\
-		fi;							\
-		echo $$i;						\
-		(cd $$i && make DIRPREFIX=${DIRPREFIX}$$i/ $@);		\
-		if [ -n "${DIRPREFIX}" ]; then				\
-			echo "<=== ${DIRPREFIX}" | sed 's!/$$!!';	\
-		fi;							\
+all:
+	@for i in ${CLASSES}; do						\
+		echo "${JAVAC} ${JFLAGS} $$i";					\
+		${JAVAC} ${JFLAGS} $$i.java || exit 1;				\
+	done
+	@# XXX: make recursion factorable
+	@for i in ${SUBDIRS}; do						\
+		echo -n "===> ";						\
+		if [ -n "${DIRPREFIX}" ]; then					\
+			echo -n ${DIRPREFIX};					\
+		fi;								\
+		echo $$i;							\
+		(cd $$i && make DIRPREFIX=${DIRPREFIX}$$i/ $@) || exit 1;	\
+		if [ -n "${DIRPREFIX}" ]; then					\
+			echo "<=== ${DIRPREFIX}" | sed 's!/$$!!';		\
+		fi;								\
+	done
+
+test:
+	@for i in ${TESTS}; do	\
+		echo $i;	\
+	done
+	@for i in ${SUBDIRS}; do						\
+		echo -n "===> ";						\
+		if [ -n "${DIRPREFIX}" ]; then					\
+			echo -n ${DIRPREFIX};					\
+		fi;								\
+		echo $$i;							\
+		(cd $$i && make DIRPREFIX=${DIRPREFIX}$$i/ $@) || exit 1;	\
+		if [ -n "${DIRPREFIX}" ]; then					\
+			echo "<=== ${DIRPREFIX}" | sed 's!/$$!!';		\
+		fi;								\
 	done

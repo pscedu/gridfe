25d1973c1327add80e6461a16a9b14bba01ee8b0
rbudden <>
2004-06-28 19:27:40 +0000

    gridint classpath additions

--- lib/mk/defs.mk
+++ lib/mk/defs.mk
@@ -3,4 +3,4 @@
 MAINMK = ${SYSROOT}/lib/mk/main.mk
 JAVAC = `which javac`
 JAVA = `which java`
-JFLAGS = -g -classpath ${SYSROOT}/lib:.
+JFLAGS = -g -classpath ${SYSROOT}/lib:/usr/share/java/servlet.jar:/usr/java/cog-1.2/lib/cog-jglobus.jar:/usr/java/j2sdk1.4.2_04/jre/lib/rt.jar:/usr/java/cog-1.2/lib/puretls.jar:/usr/java/cog-1.2/lib/cryptix32.jar:/usr/java/cog-1.2/lib/cryptix-asn1.jar:/home/rbudden/code/gridfe/lib:.

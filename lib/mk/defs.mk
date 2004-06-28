b1d76bddebcbac06ec682c6987f8594f592c948f
yanovich <>
2004-06-28 20:00:03 +0000

    make classpath more readable and portable

--- lib/mk/defs.mk
+++ lib/mk/defs.mk
@@ -3,4 +3,15 @@
 MAINMK = ${SYSROOT}/lib/mk/main.mk
 JAVAC = `which javac`
 JAVA = `which java`
-JFLAGS = -g -classpath ${SYSROOT}/lib:/usr/share/java/servlet.jar:${COG_INSTALL_PATH}/lib/cog-jglobus.jar:/usr/java/j2sdk1.4.2_04/jre/lib/rt.jar:${COG_INSTALL_PATH}/lib/puretls.jar:${COG_INSTALL_PATH}/lib/cryptix32.jar:${COG_INSTALL_PATH}/lib/cryptix-asn1.jar:.
+JAVA_INSTALL_PATH = /usr/java/j2sdk1.4.2_04
+COG_INSTALL_PATH = /usr/java/cog-1.2
+CLASSPATH =						\
+	${SYSROOT}/lib:					\
+	/usr/share/java/servlet.jar:			\
+	${JAVA_INSTALL_PATH}/jre/lib/rt.jar:		\
+	${COG_INSTALL_PATH}/lib/cog-jglobus.jar:	\
+	${COG_INSTALL_PATH}/lib/puretls.jar:		\
+	${COG_INSTALL_PATH}/lib/cryptix32.jar:		\
+	${COG_INSTALL_PATH}/lib/cryptix-asn1.jar:	\
+	.
+JFLAGS = -g -classpath `echo ${CLASSPATH} | tr -d '\t\n\ '`

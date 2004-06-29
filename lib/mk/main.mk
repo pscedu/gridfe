545284cbf473583edf83a86d367da82b9dca1337
yanovich <>
2004-06-29 14:03:30 +0000

    add clean and depend targets; make build process better

--- lib/mk/main.mk
+++ lib/mk/main.mk
@@ -1,6 +1,10 @@
 # $Id$
 
-all:
+.SUFFIXES: .class .java
+
+TARGET = ${CLASSES}
+
+all: ${TARGET}
 	@# XXX: make recursion factorable
 	@for i in ${SUBDIRS}; do						\
 		echo -n "===> ";						\
@@ -13,12 +17,28 @@ all:
 			echo "<=== ${DIRPREFIX}" | sed 's!/$$!!';		\
 		fi;								\
 	done
+
+.java.class:
+	${JAVAC} ${JCFLAGS} $< || exit 1
+
+depend:
+	@for i in ${SUBDIRS}; do						\
+		echo -n "===> ";						\
+		if [ -n "${DIRPREFIX}" ]; then					\
+			echo -n ${DIRPREFIX};					\
+		fi;								\
+		echo $$i;							\
+		(cd $$i && make DIRPREFIX=${DIRPREFIX}$$i/ $@) || exit 1;	\
+		if [ -n "${DIRPREFIX}" ]; then					\
+			echo "<=== ${DIRPREFIX}" | sed 's!/$$!!';		\
+		fi;								\
+	done
+	rm -f .depend
 	@for i in ${CLASSES}; do						\
-		echo "${JAVAC} ${JFLAGS} $$i";					\
-		${JAVAC} ${JFLAGS} $$i.java || exit 1;				\
+		echo "$$i: $${i%class}java" >> .depend;				\
 	done
 
-test: all
+test: all ${TESTS}
 	@for i in ${SUBDIRS}; do						\
 		echo -n "===> ";						\
 		if [ -n "${DIRPREFIX}" ]; then					\
@@ -31,5 +51,21 @@ test: all
 		fi;								\
 	done
 	@for i in ${TESTS}; do							\
-		echo $i;							\
+		echo "${JAVA} ${JFLAGS} $$i";					\
+		${JAVA} ${JFLAGS} $$i;						\
+	done
+
+clean:
+	@# XXX: test classes
+	rm -f ${CLASSES} .depend
+	@for i in ${SUBDIRS}; do						\
+		echo -n "===> ";						\
+		if [ -n "${DIRPREFIX}" ]; then					\
+			echo -n ${DIRPREFIX};					\
+		fi;								\
+		echo $$i;							\
+		(cd $$i && make DIRPREFIX=${DIRPREFIX}$$i/ $@) || exit 1;	\
+		if [ -n "${DIRPREFIX}" ]; then					\
+			echo "<=== ${DIRPREFIX}" | sed 's!/$$!!';		\
+		fi;								\
 	done

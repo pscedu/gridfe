0ae2a28a82a45cbb6e637f71fb43bd3df869b7e7
yanovich <>
2004-06-30 19:37:12 +0000

    initial checkin

---
+++ src/gridfe/www/logout.java
@@ -0,0 +1,23 @@
+/* $Id$ */
+
+import oof.*;
+import gridint.*;
+
+public class logout {
+	public static String main(Page page) throws Exception {
+		String s;
+		OOF oof = page.getOOF();
+		Gridint gi = page.getGridInt();
+
+		gi.auth();
+		gi.logout();
+
+		s =	page.header("Logout") +
+			oof.p("You have successfully logged out.") +
+			page.footer();
+
+		return s;
+	}
+};
+
+/* vim: set ts=4: */

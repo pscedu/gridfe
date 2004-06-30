48654076e86567ba54201dd5d4c84e3ff54624fe
yanovich <>
2004-06-30 15:49:21 +0000

    add getUserID test

--- src/jasp/t/suite.java
+++ src/jasp/t/suite.java
@@ -1,6 +1,10 @@
 /* $Id$ */
 
+import jasp.*;
+
 public class suite {
 	public static void main(String[] args) {
+		System.out.println("User ID: " + new Integer(
+			BasicServices.getUserID()));
 	}
 }

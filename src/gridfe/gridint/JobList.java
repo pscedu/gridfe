392331d5d058e7aecbea2227d16804e428f513d5
rbudden <>
2004-07-01 20:51:38 +0000

    Initial checkin

---
+++ src/gridfe/gridint/JobList.java
@@ -0,0 +1,59 @@
+/* $Id$ */
+/*
+** GridJobList -
+** Wrapper over a GridJob manager.
+** Something to maintain past & present
+** submitted GridJobs...
+*/
+
+package gridint;
+
+import gridint.*;
+import java.io.*;
+import java.util.*;
+
+public class GridJobList implements Serializable
+{
+	private List list;
+
+	public GridJobList()
+	{
+		this.list = new ArrayList();
+	}
+
+	public void push(GridJob j)
+	{
+		this.list.add(0,j);
+	}
+
+	public GridJob pop()
+	{
+		return (GridJob)(this.list.remove(0));
+	}
+
+	public boolean remove(GridJob j)
+	{
+		return this.list.remove(j);
+	}
+
+	public GridJob remove(int index)
+	{
+		return (GridJob)(this.list.remove(index));
+	}
+
+	public GridJob get(int index)
+	{
+		/* wow... this doesn't cast automatically? */
+		return (GridJob)(this.list.get(index));
+	}
+
+	public void set(int index, GridJob j)
+	{
+		this.list.set(index, j);
+	}
+
+	public int size()
+	{
+		return this.list.size();
+	}
+}

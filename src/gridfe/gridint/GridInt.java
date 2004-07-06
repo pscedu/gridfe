c18330996338ab205e77d048d08fe723829849f8
rbudden <>
2004-07-06 15:23:16 +0000

    GridJobList revive/serialize working

--- src/gridfe/gridint/GridInt.java
+++ src/gridfe/gridint/GridInt.java
@@ -3,26 +3,32 @@
 package gridint;
 
 import gridint.auth.*;
+import gridint.*;
 import jasp.Uid;
 import java.security.PrivateKey;
 import org.globus.gram.*;
 import org.globus.gsi.*;
 import org.ietf.jgss.*;
+import java.io.*;
+import java.net.MalformedURLException;
 
-public class GridInt
+public class GridInt implements Serializable
 {
-	private GlobusAuth ga;
-	private GSSAuth gss;
+	private transient GlobusAuth ga;
+	private transient GSSAuth gss;
 	private Uid uid;
+	private GridJobList list;
 
 	public GridInt(String uid)
 	{
 		this.uid = new Uid(uid);
+		this.list = new GridJobList();
 	}
 
 	public GridInt(int uid)
 	{
 		this.uid = new Uid(uid);
+		this.list = new GridJobList();
 	}
 
 	public void auth() throws GSSException, GlobusCredentialException
@@ -38,39 +44,76 @@ public class GridInt
 	{
 		job.init(this.gss.getGSSCredential());
 		job.run();
-
-		// TODO: add to linkedlist to keep track of jobs...
+		this.list.push(job);
 	}
 
-	public void jobCancel(GridJob job) throws GramException, GSSException
+	public boolean jobCancel(GridJob job) throws GramException, GSSException
 	{
 		job.cancel();
-
-		// TODO: remove from linkedlist to keep track of jobs...
+		return this.list.remove(job);
 	}
 
-	public void getJobOutput()
+	public void revive() throws MalformedURLException, GSSException, GlobusCredentialException
 	{
+		this.auth();
 
+		for(int i = 0; i < this.list.size(); i++)
+		{
+			this.list.get(i).revive(this.gss.getGSSCredential());
+		}
 	}
 
-	public void getJobStatus()
+	/*
+	** TODO: provide a wrapper over this to get the job
+	** the user wants... then call this function to
+	** read the output file (from the RSLElement)
+	** and return the data...
+	*/
+	public void getJobOutput(GridJob job)
 	{
+	}
 
+	/*
+	** TODO: elegant way to get status of jobs and retrieve
+	** the job the user wants... possibly a Map? this will
+	** be implemented when the UI is finalized
+	*/
+	/* currently get's the most recent job status */
+	public String getJobStatusAsString() throws GSSException
+	{
+		return this.list.get(0).getStatusAsString();
+	}
+	public String getJobStatusAsString(int x) throws GSSException
+	{
+		return this.list.get(x).getStatusAsString();
+	}
+	public int getJobStatus() throws GSSException
+	{
+		return this.list.get(0).getStatus();
+	}
+	public int getJobStatus(int x) throws GSSException
+	{
+		return this.list.get(x).getStatus();
 	}
 
 	
 
+	/* DEBUG */
 	public GlobusAuth getGlobusAuth()
 	{
 		return this.ga;
 	}
 
+	/* DEBUG */
 	public GSSAuth getGSSAuth()
 	{
 		return this.gss;
 	}
 
+	/* 
+	** TODO: - later there need to be lots more
+	** functions for obtaining certificate information
+	*/
 	public int getRemainingLifetime() throws GSSException
 	{
 		return this.gss.getRemainingLifetime();

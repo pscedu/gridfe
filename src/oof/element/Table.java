b8514e5acd27c73f497008a62ee5712ed36e3e12
yanovich <>
2004-07-07 17:24:17 +0000

    make rows public; change rows type to TableRow and build it there, since it cannot be built later, which would cause an exception and an unacceptable time

--- src/oof/element/Table.java
+++ src/oof/element/Table.java
@@ -5,10 +5,12 @@ import oof.*;
 import oof.element.*;
 
 public class Table extends ELEMENT implements Startable, Endable {
-	private Object[][][] rows;
+	public TableRow[] rows;
 	public Table(OOF oof, Object[] attrs, Object[][][] osss) throws OOFBadElementFormException {
 		super(oof, attrs, new Object[] {});
-		this.rows = osss;
+		this.rows = new TableRow[osss.length];
+		for (int i = 0; i < osss.length; i++)
+			this.rows[i] = new TableRow(oof, osss[i]);
 	}
 
 	public String toString() {

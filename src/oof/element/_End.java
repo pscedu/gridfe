e91bb5e608d3f70ac86d536e06a2e0abbc4aa494
yanovich <>
2004-07-07 20:20:19 +0000

    put toString in here instead of copied into each derived class; bobs bad style

--- src/oof/element/_End.java
+++ src/oof/element/_End.java
@@ -4,10 +4,27 @@ package oof.element;
 import oof.*;
 import oof.element.*;
 
-public abstract class END implements Endable {
+public abstract class END implements Endable
+{
 	public OOF oof;
-	
-	public END(OOF oof) throws OOFBadElementFormException {
+
+	public END(OOF oof) throws OOFBadElementFormException
+	{
 		this.oof = oof;
+	}
+
+	public String toString()
+	{
+		/* This would be so much easier:
+		 *	this.oof.__getFilter().build(this);
+		 */
+		try {
+			return (String)this.oof.__getFilter().getClass().getMethod("build",
+				new Class[] { this.getClass() }).invoke(
+					this.oof.__getFilter(), new Object[] { this });
+		} catch (Exception e) {
+			/* Fuck */
+			return "(@@@@@ FAILED @@@@@)";
+		}
 	}
 };

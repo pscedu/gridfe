de00711d0e3560538f8b5866381c68f67ca62078
yanovich <>
2004-07-07 20:21:32 +0000

    bobs bad style; add toString

--- src/oof/element/Startable.java
+++ src/oof/element/Startable.java
@@ -4,9 +4,11 @@ package oof.element;
 
 import java.util.*;
 
-public interface Startable {
+public interface Startable
+{
 	public void addAttribute(String key, String val);
-	public String removeAttribute(String key);
+	public Object removeAttribute(String key);
 	public String getAttribute(String key);
 	public LinkedList getAttributes();
+	public String toString();
 };

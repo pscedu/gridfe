f82bf5000d1ce6e5d727cb0653090b891213b7d7
rbudden <>
2004-07-13 14:28:46 +0000

    additional credential code

--- src/gridfe/gridint/auth/t/suite.java
+++ src/gridfe/gridint/auth/t/suite.java
@@ -6,7 +6,7 @@ import gridint.*;
 import javax.security.auth.Subject;
 import java.util.*;
 import java.security.*;
-import javax.security.auth.kerberos.KerberosKey;
+import javax.security.auth.kerberos.*;
 import java.lang.reflect.*;
 import jasp.*;
 
@@ -26,6 +26,7 @@ public class suite
 */
 
 		/* GlobusAuth Test Suite */
+		/*
 		GlobusAuth ga = new GlobusAuth(BasicServices.getUserID());
 		ga.createCredential();
 
@@ -39,47 +40,61 @@ public class suite
 		System.out.print("\n\n");
 		System.out.print(ga.getCertNum());
 		System.out.println(ga.getProxyType());
+		*/
 		
 		/* KerbAuth Test Suite */
-/*
 		Subject sub;
 		Set prin;
 		Set priv;
 		Set pub;
+		Object[] o;
+		//KerberosPrincipal[] p;
+		KerberosPrincipal p;
 		KerberosKey key;
+		
 		KerbAuth krb = new KerbAuth();
 		krb.login();
 
 		sub = krb.getSubject();
 	
-		System.out.println("-------");
+		System.out.println("---Subject----");
 		System.out.print(sub.toString());
-		System.out.println("\n-------");
-
+		System.out.println("\n---Principal----");
 		prin = sub.getPrincipals();
-
 		System.out.print(prin.toString());
-		System.out.println("\n-------");
-
+		System.out.println("\n---Public Credentials----");
 		pub = sub.getPublicCredentials();
 		System.out.print(pub.toString());
-		System.out.println("\n-------");
-
+		System.out.println("\n---Private Credentials----");
 		priv = sub.getPrivateCredentials();
+		System.out.println("Set size: "+priv.size());
 		System.out.print(priv.toString());
-		System.out.println("\n-------");
-
-		System.out.print(prin.getClass().getName());
+		System.out.println("\n----Class Names &  Other---");
+		System.out.println(prin.getClass().getName());
 		System.out.print(sub.getPrivateCredentials().getClass().getName());
-		System.out.println("\n-------");
-
-		System.out.print(prin.size()+"\n");
+		System.out.print(prin.size());
+		System.out.println("\n---Kerberos Principal----");
+		//o = (KerberosPrincipal[])prin.toArray();
+		o = (Object[])prin.toArray();
+		p = (KerberosPrincipal)o[0];
+		System.out.println(p);
+		System.out.println(p.getRealm());
+		System.out.println(p.getName());
+		System.out.println(p.getNameType());
+		System.out.println("\n---Debug----");
+		System.out.println(priv.getClass());
+		System.out.println(priv.getClass().getMethods());
+		System.out.println(priv.getClass().getFields());
+		System.out.println("Name: "+priv.getClass().getName());
+		System.out.println(priv.getClass().getDeclaringClass().getName());
+		System.out.println(priv.getClass().getDeclaringClass().getMethods());
+		System.out.println(priv.getClass().getSuperclass());
+		System.out.println(Class.forName("javax.security.auth.Subject").getFields());
 
 		krb.logout();
-*/
 
 		/* X509Auth Test Suite */
-/*		
+/*
 		int uid = 6342;
 		X509Auth krb = new X509Auth("/tmp/x509up_u" + uid);
 		krb.instantiate();

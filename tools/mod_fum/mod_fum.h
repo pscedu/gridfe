f7cc7f5034723d70f326ce3101327d7a7b0561e5
yanovich <>
2004-07-14 18:31:45 +0000

    cleanup protos; typos; remove MF_STANDALONE garbage; fix includes

--- tools/mod_fum/mod_fum.h
+++ tools/mod_fum/mod_fum.h
@@ -1,31 +1,26 @@
+/* $Id $*/
+
 /*
-** mod_fum.h
 ** Free Apache Module to provide
 ** the functionality of kinit,
 ** kx509, and kxlist -p ...
 */
 
-#include<stdio.h>
-#include<krb5.h>
-#include<time.h>
+#include <krb5.h>
 
-/* Error wrappers for running standalone */
-#define MF_STANDALONE 1
-#ifndef MF_STANDALONE
+#if 0
 #define mf_err(x,y,z) ap_log_error(APLOG_MARK,APLOG_EMERG,z,x ": Err %d", y)
 #define mf_warn(x,y,z) ap_log_error(APLOG_MARK,APLOG_WARN,z,x ": Err %d", y)
-#else
-#include<err.h>
-#define mf_err(x,y,z) errx(1,"%s: Err %d  on line %d in %s",x,y,__LINE__,__FILE__)
-#define mf_warn(x,y,z) warnx(1,"%s: Err %d on line %d in %s",x,y,__LINE__,__FILE__)
 #endif
+
+#define mf_err(x,y,z) errx(1, x ": error %d", y)
+#define mf_warn(x,y,z) warnx(x ": error %d", y)
 
 #define kProxiable 1
 #define kForwardable 0
 /* 8hrs. default */
 #define kLifetime 28800
 
-/* Kerberos 5 Instance */
 typedef struct
 {
 	krb5_context context;
@@ -35,7 +30,7 @@ typedef struct
 	
 }krb5_inst, *krb5_inst_ptr;
 
-/* Kerberos 5 Prefences */
+/* Kerberos 5 Preferences */
 typedef struct
 {
 	/*
@@ -51,9 +46,6 @@ typedef struct
 	
 }krb5_prefs, *krb5_prefs_ptr;
 
-/* Prototypes */
-void mf_kinit(krb5_inst_ptr k5, krb5_prefs_ptr kprefs);
-void mf_kinit_set_uap(krb5_prefs_ptr kprefs, char *principal, char *password);
-void mf_kinit_set_defaults(krb5_prefs_ptr kprefs);
-void mf_kinit_setup(krb5_inst_ptr k5, krb5_prefs_ptr kprefs);
-void mf_kinit_cleanup(krb5_inst_ptr k5);
+void mf_kinit(krb5_inst_ptr, krb5_prefs_ptr);
+void mf_kinit_set_uap(krb5_prefs_ptr, char *, char *);
+void mf_kinit_set_defaults(krb5_prefs_ptr);

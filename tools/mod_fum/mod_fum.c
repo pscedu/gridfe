66977a2e27f15c4eb2d2c756c8c241df9b0c38a7
rbudden <>
2004-07-14 14:31:27 +0000

    kinit finished (theoretically) - but needs compile & testing

--- tools/mod_fum/mod_fum.c
+++ tools/mod_fum/mod_fum.c
@@ -4,3 +4,174 @@
 ** the functionality of kinit,
 ** kx509, and kxlist -p ...
 */
+
+//#include"mod_fum.c"
+#include<stdio.h>
+#include<krb5.h>
+#include<time.h>
+
+/* Error Wrappers if running standalone */
+#define MF_STANDALONE 0
+#ifndef MF_STANDALONE
+#define mf_err(x,y,z) ap_log_error(APLOG_MARK,APLOG_EMERG,z,x ": Err %d", y)
+#define mf_warn(x,y,z) ap_log_error(APLOG_MARK,APLOG_WARN,z,x ": Err %d", y)
+#else
+#include<err.h>
+#define mf_err(x,y,z) errx("%s: Err %d  on line %d in %s",x,y,__FILE__,__LINE__)
+#define mf_warn(x,y,z) warnx("%s: Err %d on line %d in %s",x,y,__FILE__,__LINE__)
+#endif
+
+/* Kerberos 5 Instance */
+typedef struct
+{
+	krb5_context context;
+	krb5_ccache cache;
+	krb5_principal principal;
+	krb5_creds credentials;
+	
+}krb5_inst, *krb5_inst_ptr;
+
+/* Kerberos 5 Prefences */
+typedef struct
+{
+	/*
+	** XXX only implementing what
+	** we currently need... add support
+	** for more krb5 options later
+	*/
+	krb5_deltat lifetime;
+	krb5_deltat starttime;
+	//krb5_deltat remaintime;
+	int forwardable;
+	int proxiable;
+	char *name;
+	char *sname;
+	
+}krb5_prefs, *krb5_prefs_ptr;
+
+/* Prototypes */
+void mf_kinit(krb5_inst_ptr k5, kbr5_prefs_ptr kprefs);
+void mf_kinit_set_defaults(krb5_prefs_ptr kprefs);
+void mf_kinit_setup(krb5_inst_ptr k5, krb5_prefs_ptr kprefs);
+void mf_kinit_cleanup(krb5_inst_ptr k5);
+
+/*
+** Perform the functionality of kinit...
+*/
+void mf_kinit(krb5_inst_ptr k5, kbr5_prefs_ptr kprefs)
+{
+	krb5_error_code err;
+	krb5_get_init_creds_opt opt;
+	krb5_keytab keytab = 0;
+
+	mf_kinit_setup(k5, kprefs);
+	mf_kinit_set_defaults(kprefs);
+
+	/* Set default credential options? */
+	krb5_get_init_creds_opt_init(&opt);
+
+	/* Make our changes to the defaults */
+	krb5_get_init_creds_opt_set_forwardable(&opt, kprefs->forwardable);
+	krb5_get_init_creds_opt_set_proxiable(&opt, kprefs->proxiable);
+	krb5_get_init_creds_opt_set_tkt_life(&opt, kprefs->lifetime);
+	krb5_get_init_creds_opt_set_address_list(&opt, NULL);
+
+	/* Get the initial credentials */
+	err = krb5_get_init_creds_keytab(k5->context, &k5->credentials,
+					k5->principal, keytab,
+					kprefs->starttime,
+					kprefs->sname, &opt);
+	if(err)
+		mf_err("get initial credentials failed", err, TODO);
+		
+	/* XXX this may not be needed */
+	/* Validate the Credentials */
+	err = krb5_get_validated_creds(k5->context, &k5->credentials, k5->principal,
+					k5->cache, kprefs->sname);
+	if(err)
+		mf_err("validate credentials failed", err, TODO);
+	
+	/* Initialize the cache file */
+	err = krb5_cc_initialize(k5->context, k5->cache, k5->principal);
+	if(err)
+		mf_err("initialize cache failed", err, TODO);
+	
+	/* Store the Credential */
+	err = krb5_cc_store(k5->context, k5->cache, &k5->credentials);
+	if(err)
+		mf_err("store credentials failed", err, TODO);
+		
+	mf_kinit_cleanup(k5, kprefs);
+}
+
+void mf_kinit_set_defaults(krb5_prefs_ptr kprefs)
+{
+	kprefs->proxiable = 1;
+	kprefs->forwardable = 0;
+
+	/* 8 hrs. default */
+	kprefs->lifetime = 28800;
+}
+
+/*
+** Intial context setup
+*/
+void mf_kinit_setup(krb5_inst_ptr k5, krb5_prefs_ptr kprefs)
+{
+	krb5_error_code err; 
+
+	/* Initialize Application Context */
+	err = krb5_init_context(&k5->context)
+	if(err)
+		mf_err("krb5_init_context failed", err, TODO);
+	
+	/*
+	** Read the default credential cache:
+	** equivalent to krb5__cc_resolve(k5->context,
+	**				getenv("KRB5CACHE"),
+	**				k5->cache);
+	*/
+	err = krb5_cc_default(k5->context, &k5->cache);
+	if(err)
+		mf_err("default cache failed", err, TODO);
+	
+	/*
+	** Generate a full principal name
+	** to be used for authentication.
+	*/
+	err = krb5_sname_to_principal(k5->context,
+		NULL, NULL, KRB5_NT_SRV_HOST, &k5->principal);
+	if(err)
+		mf_err("create principal failed", err, TODO);
+	
+	/* XXX Not sure if this is needed or not.. we will see */
+	/*
+	** Take the principal name we were given and parse it
+	** into the appropriate form for authentication protocols
+	*/
+	err = krb5_parse_name(k5->context, kprefs->pname, k5->principal);
+	if(err)
+		mf_err("parse_name failed", err, TODO);
+	err = krb5_unparse_name(k5->context, k5->principcal, &k5->name);
+	if(err)
+		mf_err("unparse_name failed", err, TODO);
+}
+
+/*
+** Close and free memory
+*/
+void mf_kinit_cleanup(krb5_inst_ptr k5)
+{
+	if(k5->credentials)
+		krb5_free_creds_contents(k5->context, k5->credentials);
+	if(k5->name)
+		krb5_free_unparsed_name(k5->context, k5->name);
+	if(k5->principal)
+		krb5_free_principal(k5->context, k5->principal);
+	if(k5->cache)
+		krb5_cc_close(k5->context, k5->cache);
+	if(k5->context)
+		krb5_free_context(k5->context);
+}
+
+

fc5892984bc2ec3ce275d8eb35d25fca8bc5f700
rbudden <>
2004-07-16 16:24:30 +0000

    kxlist parse uid from ticket cache working

--- tools/mod_fum/mod_fum.c
+++ tools/mod_fum/mod_fum.c
@@ -1,9 +1,13 @@
 /* $Id$ */
 
 /*
-** Free Apache Module to provide
-** the functionality of kinit,
+-------------------------------------------------------------------
+** Free Apache Module to provide the functionality of kinit,
 ** kx509, and kxlist -p ...
+**
+** Robert Budden
+** rbudden@psc.edu
+-------------------------------------------------------------------
 */
 
 /*
@@ -11,10 +15,21 @@ XXX When deployed:
 	1) change mf_err() for apache log
 	2) change mf_dstrcpy() for apache malloc
 	3) change mf_dstrfree() for apache free
+
+Developement Notes:
+	1) find a way to combine all the krb5_init stuff that is used
+		for both kinit, and kxlist...
+
+NOTES:
+	1) since mod_fum runs under apache the env X509_USER_PROXY cannot
+		be read, therefore X.509 Certificates will be created at
+		the default location (/tmp/x509u_u####). Users who require
+		$X509_USER_PROXY to be set must do this manually.
 */
 
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
 #include<krb5.h>
 #include<kx509.h>
 #include<dlfcn.h>
@@ -28,10 +43,13 @@ static void mf_kinit_set_uap(krb5_prefs_ptr, const char*, const char*);
 static void mf_kinit_set_defaults(krb5_prefs_ptr);
 static void mf_kinit(krb5_inst_ptr, krb5_prefs_ptr);
 static void mf_get_ticket_cache(krb5_inst_ptr , char **);
+static void mf_free_ticket_cache(char *tkt_cache);
 static void mf_kx509(const char*);
+static void mf_kxlist(const char *tkt_cache);
+static int mf_get_uid_from_ticket_cache(const char *tkt);
 static char* mf_dstrcpy(const char*); 
 static void mf_dstrfree(char *s);
-static void mf_free_ticket_cache(char *tkt_cache);
+static char* mf_dstrslice(const char *s, int x, int y);
 
 /*
 ** mod_fum main... should be called by Apache
@@ -52,15 +70,17 @@ int mf_main(const char *principal, const char *password)
 
 	/* Save the tkt_cache name kinit_cleanup() */
 	mf_get_ticket_cache(&kinst, &tkt_cache);
+
+	//DEBUG
 	printf("ticket cache: %s\n",tkt_cache);
 	
 	mf_kinit_cleanup(&kinst);
 
-
 	/* kx509 - just call the kx509lib*/
 	mf_kx509(tkt_cache);
 
-	/* XXX kxlist -p */
+	/* kxlist -p */
+	mf_kxlist(tkt_cache);
 	
 	/* XXX return err if auth failed */
 
@@ -69,6 +89,69 @@ int mf_main(const char *principal, const char *password)
 	
 	return 0;
 }
+
+/*
+-------------------------------KXLIST------------------------------
+*/
+static void mf_kxlist(const char *tkt_cache)
+{
+	int uid;
+
+	/* 
+	** Parse tkt_cache name to get uid:
+	** /tmp/krb5cc_UID_FUBAR
+	** /tmp/krb5cc_UID
+	*/
+	uid = mf_get_uid_from_ticket_cache(tkt_cache);
+	if(uid == -1)
+		mf_err("get uid failed", 1, TODO);
+	
+	//DEBUG
+	printf("UID: %d\n", uid);
+
+
+	
+
+}
+
+/*
+** Parse the user id from the ticket_cache name
+*/
+static int mf_get_uid_from_ticket_cache(const char *tkt)
+{
+	int uid = -1;
+	int i, j;
+	int b, e;
+	char *suid;
+	const char *tmp = tkt;
+
+	/* default to end of string */
+	e = strlen(tkt) - 1;
+	
+	/* Grab the boundry of the uid */
+	for(i = 0, j = 0; i < (strlen(tkt) - 1) && j < 2; i++)
+	{
+		if(tkt[i] == '_')
+		{
+			if(!j)
+				b = i + 1;
+			else if(j)
+				e = i;
+			j++;
+		}
+	}
+
+
+	/* slice and convert the uid */
+	suid = mf_dstrslice(tkt, b, e);
+	if(suid)
+	{
+		uid = atoi(suid);
+	}
+
+	return uid;
+}
+
 
 /*
 -------------------------------KX509-------------------------------
@@ -96,7 +179,7 @@ static void mf_kx509(const char *tkt_cache)
 	if((do_kx509 = dlsym(h, "do_kx509")) == NULL)
 		mf_err(dlerror(),1, TODO);
 
-	/* run kx509 */
+	/* simply run kx509 */
 	err = (*do_kx509)(argc, argv);
 	dlclose(h);
 
@@ -234,21 +317,31 @@ static void mf_kinit_setup(krb5_inst_ptr k5, krb5_prefs_ptr kprefs)
 		mf_err("parse_name failed", err, TODO);
 }
 
-static void mf_kinit_cleanup(krb5_inst_ptr k5)
+static void mf_kinit_cleanup(krb5_inst_ptr kinst)
 {
-	if(&k5->credentials)
-		krb5_free_cred_contents(k5->context, &k5->credentials);
-	if(k5->principal)
-		krb5_free_principal(k5->context, k5->principal);
-	if(k5->cache)
-		krb5_cc_close(k5->context, k5->cache);
-	if(k5->context)
-		krb5_free_context(k5->context);
+	/* free mem */
+	if(&kinst->credentials)
+		krb5_free_cred_contents(kinst->context, &kinst->credentials);
+	if(kinst->principal)
+		krb5_free_principal(kinst->context, kinst->principal);
+	if(kinst->cache)
+		krb5_cc_close(kinst->context, kinst->cache);
+	if(kinst->context)
+		krb5_free_context(kinst->context);
+	
+	/* reset defaults for kxlist */
+	/*
+	&kinst->credentials = NULL;
+	kinst->principal = NULL;
+	kinst->cache = NULL;
+	kinst->context = NULL;
+	*/
 }
 
 
 /* 
 ** dynamic strcpy routine and malloc() wrapper
+** (null terminated)
 */
 static char* mf_dstrcpy(const char *s)
 {
@@ -265,6 +358,46 @@ static char* mf_dstrcpy(const char *s)
 	strncpy(s2,s,len);
 	s2[len] = '\0';
 
+	return s2;
+}
+
+/*
+** dynamic string routing to slice a section 
+*/
+static char* mf_dstrslice(const char *s, int x, int y)
+{
+	char *s2;
+	size_t len, slen;
+
+	slen = strlen(s);
+	len = y - x + 2;
+
+	if(len)
+	{
+		s2 = (char*)(malloc(sizeof(char)*len));
+
+		if(!s2)
+			mf_err("malloc failed", 1, TODO);
+
+		if(x >= 0 && y >= 0)
+		{
+			if(x < slen && y < slen)
+			{
+				/* ptr increment to start location */
+				s += x;
+				
+				strncpy(s2, s, len - 1);
+				s2[len-1] = '\0';
+			}
+			else
+			{
+				//mf_err("bounds out of rangeg", 1, TODO);
+				free(s2);
+				s2 = NULL;
+			}
+		}
+	}	
+	
 	return s2;
 }
 

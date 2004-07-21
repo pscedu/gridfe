5a14df3361135c63f23d131829659ccf5be93632
yanovich <>
2004-07-21 14:47:16 +0000

    spacing; add index handler

--- src/gridfe/GridFE.java
+++ src/gridfe/GridFE.java
@@ -36,8 +36,10 @@ public class GridFE extends HttpServlet
 	private HttpServletResponse res;
 
 	final DelegationHandler[] dtab = new DelegationHandler[] {
-		new DelegationHandler("/login",  gridfe.www.login.class),
-		new DelegationHandler("/logout", gridfe.www.logout.class)
+		new DelegationHandler("",		gridfe.www.index.class),
+		new DelegationHandler("/",		gridfe.www.index.class),
+		new DelegationHandler("/login",		gridfe.www.login.class),
+		new DelegationHandler("/logout",	gridfe.www.logout.class)
 	};
 
 	public void doGet(HttpServletRequest req, HttpServletResponse res)

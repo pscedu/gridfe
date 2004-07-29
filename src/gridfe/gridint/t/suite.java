3caaffefa5480b52a081ad30c191209969c57d1b
rbudden <>
2004-07-29 21:04:37 +0000

    removed old deprecated test code

--- src/gridfe/gridint/t/suite.java
+++ src/gridfe/gridint/t/suite.java
@@ -28,48 +28,6 @@ public class suite
 //		System.out.println(gi.getGlobusAuth().getSubject());
 		System.out.println(gi.getName());
 
-		/* RSLElement Test */
-//		RSLElement r;
-//		r = new RSLElement(new String[] {"executable","stdout"},
-//				new String[] {"/bin/date", "job-output.gram"},
-//				new String("arguments"),
-//				new String[] {"-arg1", "-arg2 test"});
-
-//		System.out.println(r);
-
-//		r = new RSLElement(new String[] {"executable","stdout"},
-//				new String[] {"/bin/date", "job-output.gram"},
-//				new String("environment"),
-//				new String[] {"env1", "env2"},
-//				new String[] {"value1", "value2"});
-
-//		System.out.println(r);
-
-//		r = new RSLElement(new String[] {"executable","stdout"},
-//				new String[] {"/bin/date", "job-output.gram"},
-//				new String("arguments"),
-//				new String[] {"-arg1", "-arg2 test"},
-//				new String("environment"),
-//				new String[] {"env1", "env2"},
-//				new String[] {"value1", "value2"});
-
-//		System.out.println(r);
-
-		/* Use RSLElement for GRAM test */
-//		RSLElement rsl;
-//		rsl = new RSLElement(new String[] {"executable","stdout", "stderr"},
-//				new String[] {"/bin/uname", "/home/rbudden/job-output.gram", "/home/rbudden/job-err.gram"},
-//				new String("arguments"),
-//				new String[] {"-a"});
-
-//		r = new RSLElement(new String[] {"executable", "stdout"}, new String[] {"/bin/sleep", "gram.out"},
-//					new String("arguments"), new String[] {"10s"});
-
-		/* Other Method tests */
-//		System.out.println(gri.getJob().getID());
-//		System.out.println(gri.getIDAsString());
-//		System.out.println("Stdout: " + gri.getStdout());
-
 		/* Create a new job */
 		System.out.println("Creating New Job...");
 
@@ -113,28 +71,16 @@ public class suite
 		gi = (GridInt)(in.readObject());
 
 		in.close();
-		//gi.revive();
-
-		/* 
-		** Revival Test
-		** make sure we can recreate GridJobs from stored values
-		** and gss credentials. (this is actually a clone test now
-		** that deserialization has been implemented)
-		*/
-		//GridJob jc = new GridJob();
-		//jc.revive(j.getHost(), j.getIDAsString(), gi.getGSSAuth().getGSSCredential(), j.getRSL());
 
 		do
 		{
 			System.out.println("J1: "+gi.getJobStatus()+" : "+gi.getJobStatusAsString());
 			System.out.println("J2: "+gi.getJobStatus(1)+" : "+gi.getJobStatusAsString(1));
-			//System.out.println("JC: "+j2.getStatus()+" : "+jc.getStatusAsString());
 			Thread.sleep(600);
 
 		}while(gi.getJobStatus() != -1 || gi.getJobStatus(1) != -1);
 
 		System.out.println("J1: "+gi.getJobStatus()+" : "+gi.getJobStatusAsString());
 		System.out.println("J2: "+gi.getJobStatus(1)+" : "+gi.getJobStatusAsString(1));
-		//System.out.println("JC: "+j2.getStatus()+" : "+jc.getStatusAsString());
 	}
 };

1b9408a56ac0776d2dbe6f7c14a0a7769cef6367
rbudden <>
2004-08-06 20:04:25 +0000

    update for GassInt and CertInfo

--- src/gridfe/gridint/t/suite.java
+++ src/gridfe/gridint/t/suite.java
@@ -22,11 +22,22 @@ public class suite
 		//GridInt gi = new GridInt(BasicServices.getUserID());
 		GridInt gi = new GridInt(6342);
 		gi.auth();
+
+		CertInfo ci;
+		ci = gi.getCertInfo();
+
 		System.out.print("Remaining Lifetime: ");
-		System.out.println(gi.getRemainingLifetime());
-//		System.out.println(gi.getGlobusAuth().getCredential().getPrivateKey());
-//		System.out.println(gi.getGlobusAuth().getSubject());
-		System.out.println(gi.getName());
+		System.out.println(ci.time);
+		System.out.print("Subject: ");
+		System.out.println(ci.sub);
+		System.out.print("Identity: ");
+		System.out.println(ci.ident);
+		System.out.print("Issuer: ");
+		System.out.println(ci.issuer);
+		System.out.print("KeyStrength: ");
+		System.out.println(ci.key);
+		System.out.print("Name: ");
+//		System.out.println(gi.getName());
 
 		/* Create a new job */
 		System.out.println("Creating New Job...");
@@ -45,7 +56,7 @@ public class suite
 
 		/* test output permission */
 		String j3_out = new String("/tmp/gram.out.date");
-		String j3_host = new String("mugatu.psc.edu");
+		String j3_host = new String("intel2.psc.edu");
 		GridJob j3 = new GridJob(j3_host);
 		j3.setRSL(new String[] {"executable", "stdout"},
 			new String[] {"/bin/date", j3_out});
@@ -53,67 +64,69 @@ public class suite
 
 		/* Submit the job to GRAM */
 		System.out.println("Submiting Job...");
-//		gi.jobSubmit(j);
-//		gi.jobSubmit(j2);
-		gi.jobSubmit(j3);
+		gi.jobSubmit(j);
+		gi.jobSubmit(j2);
+	//	gi.jobSubmit(j3);
 
 		/* Print the job id string */
-//		System.out.println("j - id string: "+j.getIDAsString());
-//		System.out.println("j2 - id string: "+j2.getIDAsString());
+		System.out.println("j - id string: "+j.getIDAsString());
+		System.out.println("j2 - id string: "+j2.getIDAsString());
 
 		/* Test Serialization */
-//		System.out.println("Serializing Job...");
-//		FileOutputStream fout = new FileOutputStream("job.revive");
-//		ObjectOutputStream out = new ObjectOutputStream(fout);
-//		out.writeObject(gi);
-//		out.close();
+		System.out.println("Serializing Job...");
+		FileOutputStream fout = new FileOutputStream("job.revive");
+		ObjectOutputStream out = new ObjectOutputStream(fout);
+		out.writeObject(gi);
+		out.close();
 
 		/* Explicitly get rid of this object! */
-//		System.out.println("Removing Instance of Job...");
-//		WeakReference r = new WeakReference(gi);
-//		r.clear();
-//		gi = null;
-//		j = null;
-//		j2 = null;
+		System.out.println("Removing Instance of Job...");
+		WeakReference r = new WeakReference(gi);
+		r.clear();
+		gi = null;
+		j = null;
+		j2 = null;
 
 		/* Test Deserialization */
-//		System.out.println("Deserializing Job...");
-//		FileInputStream fin = new FileInputStream("job.revive");
-//		ObjectInputStream in = new ObjectInputStream(fin);
+		System.out.println("Deserializing Job...");
+		FileInputStream fin = new FileInputStream("job.revive");
+		ObjectInputStream in = new ObjectInputStream(fin);
 
-//		gi = (GridInt)(in.readObject());
+		gi = (GridInt)(in.readObject());
 
-//		in.close();
+		in.close();
 
 		/* Monitor job status */
-//		do
-//		{
-//			System.out.println("J1: "+gi.getJobStatus()+" : "+gi.getJobStatusAsString());
-//			System.out.println("J2: "+gi.getJobStatus(1)+" : "+gi.getJobStatusAsString(1));
-//			Thread.sleep(600);
+		do
+		{
+			System.out.println("J1: "+gi.getJobStatus()+" : "+gi.getJobStatusAsString());
+			System.out.println("J2: "+gi.getJobStatus(1)+" : "+gi.getJobStatusAsString(1));
+			Thread.sleep(600);
 
-//		}while(gi.getJobStatus() != -1 || gi.getJobStatus(1) != -1);
+		}while(gi.getJobStatus() != -1 || gi.getJobStatus(1) != -1);
 
-//		System.out.println("J1: "+gi.getJobStatus()+" : "+gi.getJobStatusAsString());
-//		System.out.println("J2: "+gi.getJobStatus(1)+" : "+gi.getJobStatusAsString(1));
+		System.out.println("J1: "+gi.getJobStatus()+" : "+gi.getJobStatusAsString());
+		System.out.println("J2: "+gi.getJobStatus(1)+" : "+gi.getJobStatusAsString(1));
 
 		/* Use a GassInt to grab job output */
+	/*
 		GassInt gass = new GassInt(gi.getCredential(), j3_host, 28003);
 		System.out.println("Starting Remote Gass Server");
 		gass.start();
+		gass.start_remote();
 		System.out.println("Attempting to open file: "+j3_out);
 		gass.open(j3_out);
 		System.out.println("Size: "+gass.getSize());
-
+	*/
 		/* Grab the job output */
-		System.out.println("Reading file...");
+	/*	System.out.println("Reading file...");
 		String data = gass.read();
 		System.out.println(data);
 
 		gass.close();
 		gass.shutdown();
 		System.out.println("Gass Server shutdown");
-
+	*/
 		/* - this shows that the gass server does terminate properly...
 		if(gass.shutdown())
 			System.out.println("Shutdown completed");

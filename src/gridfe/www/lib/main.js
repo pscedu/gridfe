d0b8415f66f38847c97b20dccd02765887b46bf4
yanovich <>
2004-08-09 20:48:07 +0000

    actually use the right position variable

--- src/gridfe/www/lib/main.js
+++ src/gridfe/www/lib/main.js
@@ -6,15 +6,15 @@ var MI_NAME = 0
 var MI_SUBMENU = 1
 
 function moveMenu() {
-	var s, x
+	var s, y
 	for (var i in menus) {
 		var o = objGet(menus[i][MI_NAME])
 		if (o != null && o.style != null) {
-			var y = objGetTop(o) + MENU_INIT_SHIFT
+			y = objGetTop(o) + MENU_INIT_SHIFT
 			objSetTop(o, y)
-			if (x < 0)
+			if (y < 0)
 				window.setTimeout('moveMenu()', MENU_INIT_INTV)
-			if (x > -80)
+			if (y > -80)
 				objSetVisibility(o, CSS_VISIBILITY_VISIBLE)
 		}
 	}

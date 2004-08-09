d19ccce806dca080a82519751d8f3f33f6a33690
yanovich <>
2004-08-09 15:52:42 +0000

    typo

--- src/gridfe/www/lib/Browser.js
+++ src/gridfe/www/lib/Browser.js
@@ -1,7 +1,7 @@
 /* $Id$ */
 
 function Browser() {
-	this.isNN4 = (navigator.appName = 'Netscape' &&
+	this.isNN4 = (navigator.appName == 'Netscape' &&
 			parseInt(navigator.appVersion) == 4)
 	this.hasDOM = document.getElementById
 	return this

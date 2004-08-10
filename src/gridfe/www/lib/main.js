33785a4104d2499915e78e0e55c0f0bdafd3c6e1
yanovich <>
2004-08-10 17:25:26 +0000

    add menu show/hide code

--- src/gridfe/www/lib/main.js
+++ src/gridfe/www/lib/main.js
@@ -4,6 +4,7 @@ var MENU_INIT_INTV = 40
 var MENU_INIT_SHIFT = 5
 var MI_NAME = 0
 var MI_SUBMENU = 1
+var MENU_HIDE_TIMO = 1000	/* Milliseconds. */
 
 function moveMenu() {
 	var s, y
@@ -22,4 +23,31 @@ function moveMenu() {
 
 window.onload = function() {
 	window.setTimeout('moveMenu()', MENU_INIT_INTV)
+}
+
+var menutimo = null
+
+function showMenu(m) {
+	setMenuDisplay(m.id, 'normal')
+	if (menutimo != null) {
+		window.clearTimeout(menutimo)
+		menutimo = null
+	}
+}
+
+function hideMenu(m) {
+	var code = ''
+	
+	code += 'setMenuDisplay(m.id, "none");'
+	code += 'menutimo = null;'
+	menutimo = window.setTimeout(code, MENU_HIDE_TIMO)
+}
+
+function setMenuDisplay(m, d) {
+	var o
+	for (var i in menus[m]) {
+		o = objGet(menus[m][i])
+		if (o != null)
+			objSetDisplay(o, d)
+	}
 }

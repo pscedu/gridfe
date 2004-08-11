1104133a78354bcb939ce26d0265bd87fe2d9e82
yanovich <>
2004-08-11 20:45:47 +0000

    overhaul; add menuGet; add animation to menuSetDisplay for showing/hiding of sub-menus; fix timeout handling in menuHide and menuShow; add halting of initial animation after first page load; turn timeout handling into an array to allow multi sub-menus to work correctly; add new consts for timeout intervals

--- src/gridfe/www/lib/main.js
+++ src/gridfe/www/lib/main.js
@@ -2,14 +2,16 @@
 
 var MENU_INIT_INTV = 40
 var MENU_INIT_SHIFT = 5
-var MI_NAME = 0
-var MI_SUBMENU = 1
-var MENU_HIDE_TIMO = 1000	/* Milliseconds. */
+var MI_NAM = 0
+var MI_SUB = 1
+var MENU_HIDE_TIMO = 500	/* Milliseconds. */
+var MENU_SHOW_TIMO = 300	/* Milliseconds. */
+var MENU_DISP_INTV = 50		/* Milliseconds. */
 
 function menuMove() {
 	var s, y
 	for (var i in menus) {
-		var o = objGet(menus[i][MI_NAME])
+		var o = objGet(menus[i][MI_NAM])
 		if (o != null && o.style != null) {
 			y = objGetTop(o) + MENU_INIT_SHIFT
 			objSetTop(o, y)
@@ -22,32 +24,76 @@ function menuMove() {
 }
 
 window.onload = function() {
+	if (document.cookie != null)
+		MENU_INIT_INTV = 0
 	window.setTimeout('menuMove()', MENU_INIT_INTV)
 }
 
-var menutimo = null
+var menutimo = []
 
-function menuShow(m) {
-	menuSetDisplay(m.id, 'normal')
-	if (menutimo != null) {
-		window.clearTimeout(menutimo)
-		menutimo = null
+function menuShow(o) {
+	var m = menuGet(o.id)
+
+	if (m != null) {
+		var nam = m[MI_NAM]
+		var code = ''
+
+		/* XXX: race condition. */
+		if (menutimo[nam] != null)
+			window.clearTimeout(menutimo[nam])
+
+		code += 'menuSetDisplay("' + nam + '", "block");'
+		menutimo[nam] = window.setTimeout(code, MENU_SHOW_TIMO)
 	}
 }
 
-function menuHide(m) {
+function menuHide(o) {
+	var m = menuGet(o.id)
+
+	if (m != null) {
+		var nam = m[MI_NAM]
+		var code = ''
+
+		/* XXX: race condition. */
+		if (menutimo[nam] != null)
+			window.clearTimeout(menutimo[nam])
+
+		code += 'menuSetDisplay("' + nam + '", "none");'
+		menutimo[nam] = window.setTimeout(code, MENU_HIDE_TIMO)
+	}
+}
+
+function menuSetDisplay(nam, d) {
+	var o, m, next = null
 	var code = ''
-	
-	code += 'menuSetDisplay(m.id, "none");'
-	code += 'menutimo = null;'
-	menutimo = window.setTimeout(code, MENU_HIDE_TIMO)
+
+	m = menuGet(nam)
+	if (m != null) {
+		if (d == 'none')
+			m[MI_SUB].reverse()	/* XXX */
+		for (var i in m[MI_SUB]) {
+			o = objGet(m[MI_SUB][i])
+			if (o != null)
+				if (objGetDisplay(o) != d) {
+					next = o
+					break
+				}
+		}
+		if (d == 'none')
+			m[MI_SUB].reverse()	/* XXX */
+		if (next == null)
+			menutimo[nam] = null
+		else {
+			objSetDisplay(next, d)
+			code += 'menuSetDisplay("' + nam + '", "' + d + '")'
+			menutimo[nam] = window.setTimeout(code, MENU_DISP_INTV)
+		}
+	}
 }
 
-function menuSetDisplay(m, d) {
-	var o
-	for (var i in menus[m]) {
-		o = objGet(menus[m][i])
-		if (o != null)
-			objSetDisplay(o, d)
-	}
+function menuGet(nam) {
+	for (var i in menus)
+		if (menus[i][MI_NAM] == nam)
+			return (menus[i])
+	return (null)
 }

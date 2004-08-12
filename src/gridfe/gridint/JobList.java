b6ef5472f6a5ef789b5957e733c6731e81d576ea
rbudden <>
2004-08-12 17:54:54 +0000

    update to use both Hashtable and ArrayList to keep track of jobs by name

--- src/gridfe/gridint/JobList.java
+++ src/gridfe/gridint/JobList.java
@@ -6,52 +6,80 @@ import gridfe.gridint.*;
 import java.io.*;
 import java.util.*;
 
+/*
+** JobList combines both a hash table and an
+** array list.  This allows us to keep track of jobs
+** by both order and name.
+*/
 public class JobList implements Serializable
 {
 	private List list;
+	private Hashtable table;
 
 	public JobList()
 	{
 		this.list = new ArrayList();
+		this.table = new Hashtable();
 	}
 
 	public void push(GridJob j)
 	{
-		this.list.add(0, j);
-	}
+		/* Add to end of list */
+		this.list.add(j);
 
+		/* Save index into hashtable */
+		this.table.put(j.getName(), new Integer(this.list.size() - 1));
+	}
+/*
 	public GridJob pop()
 	{
 		return (GridJob)(this.list.remove(0));
 	}
-
+*/
 	public boolean remove(GridJob j)
 	{
+		this.table.remove(j.getName());
+
 		return this.list.remove(j);
 	}
-
+/*
 	public GridJob remove(int index)
 	{
 		return (GridJob)(this.list.remove(index));
 	}
-
+*/
 	public GridJob get(int index)
 	{
 		return (GridJob)(this.list.get(index));
 	}
+	
+	public GridJob get(String name)
+	{
+		/* Grab the index from the hashtable */
+		int index = ((Integer)(this.table.get(name))).intValue();
 
+		/* Then grab the job */
+		return (GridJob)(this.list.get(index));
+	}
+/*
 	public void set(int index, GridJob j)
 	{
 		this.list.set(index, j);
 	}
-
+*/
 	public int size()
 	{
 		return this.list.size();
 	}
-
+/*
 	public void clear()
 	{
 		this.list.clear();
+	}
+*/
+	public Enumeration keys()
+	{
+		/* Return the list of keys */
+		return this.table.keys();
 	}
 };

296025e9bd80ca3f36f9c5499e8208ab0d73054a
rbudden <>
2004-08-13 21:28:57 +0000

    first attempt at organizing the information of the certificate

--- src/gridfe/www/certs.java
+++ src/gridfe/www/certs.java
@@ -2,6 +2,8 @@
 
 package gridfe.www;
 
+import gridfe.gridint.auth.*;
+import gridfe.gridint.*;
 import gridfe.*;
 import oof.*;
 
@@ -12,8 +14,41 @@ public class certs
 	{
 		OOF oof = page.getOOF();
 
+		int uid = 6342;
+//		int uid = page.getUserid();
+		CertInfo ci;
+
+		GridInt gi = new GridInt(uid);
+		gi.auth();
+
+		ci = gi.getCertInfo();
+
+		long tmp;
+		long sec = ci.time;
+		long days = (sec / (tmp = 24*60*60));
+		sec -= days * tmp;
+		long hours = (sec / (tmp = 60*60));
+		sec -= hours * tmp;
+		long min = (sec / 60);
+		sec -= min * 60;
+
 		return	page.header("Certificate Management") +
-				oof.p("Certificate management test page.") +
+//				oof.p("Certificate management test page.") +
+				oof.p("- Location -") +
+				oof.p("Kerberos Ticket:\t" + ci.kfile) +
+				oof.p("X.509 Certificate:\t" + ci.xfile) +
+				oof.p("") +
+				oof.p("- Contents -") +
+				oof.p("Issuer:\t" + ci.issuer) +
+				oof.p("Subject:\t" + ci.sub) +
+				oof.p("Identity:\t" + ci.ident) +
+				oof.p("Type:\t\t" + ci.type) +
+				oof.p("Key Strength:\t" + ci.key + "bit") +
+				oof.p("Remaining Lifetime:\t" + ci.time + 
+						" ("+ days + " Days, " + hours + 
+						" Hours, " + min + " Minuets, " +
+						sec + " Seconds)") +
+				oof.p("") +
 				page.footer();
 	}
 };

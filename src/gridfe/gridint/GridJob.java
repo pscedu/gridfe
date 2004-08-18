2fd02f32e494a668af7b85bc460ad748036b4a4f
rbudden <>
2004-08-18 14:47:03 +0000

    fix error (localhost != host), add GRAM -> GASS filename conversion

--- src/gridfe/gridint/GridJob.java
+++ src/gridfe/gridint/GridJob.java
@@ -84,6 +84,14 @@ public class GridJob extends RSLElement implements Serializable
 		*/
 		int which = 0;
 
+		/* Job submitted to local machine */
+		boolean local = false;
+
+		/* XXX - Check for host to be localhost */
+//		if(this.host.equals(BasicServices.getLocalhost()))
+		if(this.host.equalsIgnoreCase("gridinfo.psc.edu"))
+			local = true;
+
 		/* Check for starting 'http:' or 'https:' */
 		if(this.stdout != null)
 			if(this.stdout.startsWith("http:") ||
@@ -92,10 +100,62 @@ public class GridJob extends RSLElement implements Serializable
 
 		if(this.stderr != null)
 			if(this.stderr.startsWith("http:") ||
-			   this.stdout.startsWith("https:"))
-				which += 2;	
+			   this.stderr.startsWith("https:"))
+				which += 2;
+
+		/*
+		** If the job is remote, the output
+		** is automatically remote whether it
+		** via a gass server or not
+		*/
+		/* XXX - if it is not local submitted it
+		** could be a job submitted to intel2 with output
+		** going to rachel... need a way to extract the
+		** proper host, etc...
+		*/
+		if(!local) 
+			which = 3;
 		
 		return which;
+	}
+
+	/* Convert GRAM stdout, and directory to a GASS filename */
+	public String convert(String file)
+	{
+		String dir = null;
+
+		/*
+		** Determine if directory needs prepended to output.
+		** If std(out/err) string starts with a '/' or '~'
+		** then the user has explicitly stated the path.
+		** If directory does not start with '/' then it
+		** needs to default to "~".
+		*/
+		if(file != null)
+		{
+			dir = (file.charAt(0) != '/') ? "~" : "";
+			dir += (this.dir != null) ? "/" + this.dir : "";
+		}
+
+		/*
+		** Unfortunately GRAM assumes directories start from
+		** ~/ and if ~/dir is specified GRAM cannot expand the ~
+		**
+		** GASS on the other hand seems to assume a full path
+		** and support ~ expansion via the TILDE_EXPAND_ENABLE
+		** option.
+		**
+		** Therefore we have to manually adjust stdout, stderr
+		** and directory accordingly.
+		*/
+		if(file != null && file.charAt(0) != '/' &&
+			file.charAt(0) != '~')
+		{
+			file = dir + "/" + file;
+		}
+
+		System.out.println(file);
+		return file;
 	}
 
 	/* GramInt Wrappers */

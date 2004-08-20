87036c049cdac7c54b851d0a318210fdcaee78ac
rbudden <>
2004-08-20 16:32:32 +0000

    added todo, note about mod_fum change

--- src/gridfe/gridint/auth/CertFile.java
+++ src/gridfe/gridint/auth/CertFile.java
@@ -13,6 +13,19 @@ public class CertFile
 	*/
 	private final String xdef = "/tmp/x509up_u";
 	private final String kdef = "/tmp/krb5cc_";
+
+	/*
+	** These are what mod_fum creates so they dont
+	** conflict with console login credentials that
+	** may exist already... Use these when deployed.
+	**
+	** XXX - put this stuff in some configuration
+	** file to be read in... kinda nasty hard coded
+	** the way it is for developement purposes!
+	*/
+	//private final String xdef = "/tmp/x509up_fum_u";
+	//private final String kdef = "/tmp/krb5cc_fum_";
+
 	private String xfile;
 	private String kfile;
 

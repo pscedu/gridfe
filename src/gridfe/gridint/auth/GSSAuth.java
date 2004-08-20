fe05fd8e7e5c18b3623bed80d37f89e67152c372
rbudden <>
2004-08-20 18:30:47 +0000

    comment error

--- src/gridfe/gridint/auth/GSSAuth.java
+++ src/gridfe/gridint/auth/GSSAuth.java
@@ -33,8 +33,9 @@ public class GSSAuth
 		** GlobusCredential to GSSCredential... However,
 		** this is broken in CoG 1.1 (and previous also
 		** I assume)... In order for this to work properly
-		** CoG jglobus was compiled from the "CoG 1.0 pre alpha"
-		** source code. (cvs.globus.org)
+		** CoG jglobus was compiled from the "CoG 2.0 pre alpha"
+		** source code. (cvs.globus.org) How there is such a
+		** thing as pre alpha is beyond me...
 		*/
 		this.gss = new GlobusGSSCredentialImpl(this.gc,
 				GSSCredential.INITIATE_AND_ACCEPT);

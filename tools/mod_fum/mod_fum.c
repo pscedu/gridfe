bd19bfcd780ce0a99c6ac98d7064397bf7d13eb2
yanovich <>
2004-08-30 14:37:16 +0000

    knf

--- tools/mod_fum/mod_fum.c
+++ tools/mod_fum/mod_fum.c
@@ -140,43 +140,43 @@ mod_fum_auth(request_rec *r)
 	err = ap_get_basic_auth_pw(r, &pass);
 	user = r->user;
 
-	if (err == OK && user && pass) {
-		err = HTTP_UNAUTHORIZED;
-
-		/* Check if previous credentials exist */
-		if (mf_check_for_cred(user)) {
-			/*
-			 * If they exist, make sure the user/pass is correct;
-			 * otherwise, a correct username and wrong password
-			 * will work.
-			 */
-			if (mf_valid_user(user, pass)) {
-				/*
-				 * Finally check if the credentials have expired
-				 * or not.  If so create new certs, if not, do
-				 * nothing.
-				 */
-				if (mf_valid_cred(user))
-					/* Create new certs */
-					err = mod_fum_main(user, pass);
-				else
-					/*
-					 * XXX This could also be a permissions
-					 * problem.  If the user has already a
-					 * valid X.509 certificate, Apache does
-					 * not have permissions to read it.
-					 */
-					mf_log("credentials expired");
-			} else
-				mf_log("wrong user/pass combination");
-		} else
-			/* Create new certs */
-			err = mod_fum_main(user, pass);
-	} else {
+	if (err != OK || user == NULL || pass == NULL) {
 		mf_log("authentication incomplete (%d)", err);
-		err = HTTP_UNAUTHORIZED;
+		return (HTTP_UNAUTHORIZED);
 	}
-	return (err);
+
+	/* Check if previous credentials exist */
+	if (!mf_check_for_cred(user))
+		/* Create new certs */
+		return (mod_fum_main(user, pass));
+
+	/*
+	 * They exist, make sure the user/pass is correct;
+	 * otherwise, a correct username and wrong password
+	 * will work.
+	 */
+	if (!mf_valid_user(user, pass)) {
+		mf_log("wrong user/pass combination");
+		return (HTTP_UNAUTHORIZED);
+	}
+
+	/*
+	 * Finally check if the credentials have expired
+	 * or not.  If so, create new certs; if not, do
+	 * nothing.
+	 */
+	if (!mf_valid_cred(user)) {
+		/*
+		 * XXX This could also be a permissions problem.  If the
+		 * user has already a valid X.509 certificate, Apache
+		 * does not have permission to read it.
+		 */
+		mf_log("credentials expired");
+		return (HTTP_UNAUTHORIZED);
+	}
+
+	/* Create new certs */
+	return (mod_fum_main(user, pass));
 }
 
 int
@@ -190,7 +190,7 @@ mod_fum_main(const char *principal, const char *password)
 	/* XXX Resolve UID from KDC with given principal (possible?) */
 
 	/* Read uid from /etc/passwd */
-	if ((err = mf_user_id_from_principal(principal, &uid)) != OK)
+	if ((err = mf_user_id_from_principal(principal, &uid)) != 0)
 		return (err);
 	if ((tkt_cache = mf_dstrcat(_PATH_KRB5CERT, uid)) == NULL) {
 		mf_log("tkt_cache is NULL");
@@ -198,7 +198,7 @@ mod_fum_main(const char *principal, const char *password)
 	}
 	free(uid);
 
-	if ((err = mf_krb5_init(&ki, tkt_cache)) != OK)
+	if ((err = mf_krb5_init(&ki, tkt_cache)) != 0)
 		return (err);
 
 	kp.kp_proxy = KPD_PROXIABLE;
@@ -207,17 +207,17 @@ mod_fum_main(const char *principal, const char *password)
 	kp.kp_prin = principal;
 	kp.kp_pw = password;
 
-	if ((err = mf_kinit_setup(&ki, &kp)) != OK)
+	if ((err = mf_kinit_setup(&ki, &kp)) != 0)
 		return (err);
 
 	/* kinit -c /tmp/krb5cc_$UID */
-	if ((err = mf_kinit(&ki, &kp)) != OK)
+	if ((err = mf_kinit(&ki, &kp)) != 0)
 		return (err);
 
 	mf_kinit_cleanup(&ki);
 	mf_krb5_free(&ki);
 
-	if ((err = mf_kx509(tkt_cache)) != OK)
+	if ((err = mf_kx509(tkt_cache)) != 0)
 		return (err);
 
 	/* kxlist -p */
@@ -233,27 +233,24 @@ mf_kxlist(const char *tkt_cache)
 
 	/*
 	 * Parse tkt_cache name to get UID:
-	 * /tmp/krb5cc_UID_FUBAR
-	 * /tmp/krb5cc_UID
+	 *	/tmp/krb5cc_UID_FUBAR
+	 *	/tmp/krb5cc_UID
 	 */
-	uid = mf_get_uid_from_ticket_cache(tkt_cache);
-	if (uid) {
-		/* krb5 initial context setup */
-		err = mf_krb5_init(&ki, tkt_cache);
-		if (err == OK) {
-			/* Obtain proper kx509 credentials */
-			if ((err = mf_kxlist_setup(&ki)) != OK)
-				return (err);
-
-			/* Perform crypto & write certificate */
-			name = mf_dstrcat(_PATH_X509CERT, uid);
-			if (name == NULL)
-				return (HTTP_INTERNAL_SERVER_ERROR);
-			err = mf_kxlist_crypto(&ki, name);
-			mf_krb5_free(&ki);
-		}
-	} else
+	if ((uid = mf_get_uid_from_ticket_cache(tkt_cache)) == 0)
 		return (HTTP_INTERNAL_SERVER_ERROR);
+	/* krb5 initial context setup */
+	if ((err = mf_krb5_init(&ki, tkt_cache)) != 0)
+		return (err);
+	/* Obtain proper kx509 credentials */
+	if ((err = mf_kxlist_setup(&ki)) != 0)
+		return (err);
+
+	/* Perform crypto & write certificate */
+	name = mf_dstrcat(_PATH_X509CERT, uid);
+	if (name == NULL)
+		return (HTTP_INTERNAL_SERVER_ERROR);
+	err = mf_kxlist_crypto(&ki, name);
+	mf_krb5_free(&ki);
 	return (err);
 }
 
@@ -267,39 +264,37 @@ mf_kxlist_crypto(struct krb5_inst *ki, char *name)
 	unsigned char *data;
 	X509 *cert = NULL;
 	RSA *priv = NULL;
-	int err = OK;
 	FILE *file;
 
 	if ((file = fopen(name, "w")) == NULL) {
 		mf_log("%s", name);
 		return (HTTP_INTERNAL_SERVER_ERROR);
-	} else {
-		klen = ki->ki_cred.ticket.length;
-		clen = ki->ki_cred.second_ticket.length;
-
-		/* Decode the certificate (we want PEM format) */
-		data = ki->ki_cred.second_ticket.data;
-		d2i_X509((X509**)(&cert), &data, clen);
-
-		/* Extract & decode the RSA private key from the certificate */
-		data = ki->ki_cred.ticket.data;
-		d2i_RSAPrivateKey(&priv, (const unsigned char **)(&data), klen);
-
-		if (priv == NULL) {
-			mf_log("d2i_RSAPrivateKey failed");
-			err = HTTP_INTERNAL_SERVER_ERROR;
-		} else {
-			/* Write the certificate appropriately formatted */
-			PEM_write_X509(file, cert);
-			PEM_write_RSAPrivateKey(file, priv, NULL, NULL, 0, NULL,
-			    NULL);
-			(void)fclose(file);
-
-			/* Set proper permissions */
-			(void)chmod(name, X509_FILE_PERM);
-		}
 	}
-	return (err);
+	klen = ki->ki_cred.ticket.length;
+	clen = ki->ki_cred.second_ticket.length;
+
+	/* Decode the certificate (we want PEM format) */
+	data = ki->ki_cred.second_ticket.data;
+	d2i_X509((X509**)(&cert), &data, clen);
+
+	/* Extract & decode the RSA private key from the certificate */
+	data = ki->ki_cred.ticket.data;
+	d2i_RSAPrivateKey(&priv, (const unsigned char **)(&data), klen);
+
+	if (priv == NULL) {
+		mf_log("d2i_RSAPrivateKey failed");
+		return (HTTP_INTERNAL_SERVER_ERROR);
+	}
+
+	/* Write the certificate, appropriately formatted */
+	PEM_write_X509(file, cert);
+	PEM_write_RSAPrivateKey(file, priv, NULL, NULL, 0, NULL,
+	    NULL);
+	(void)fclose(file);
+
+	/* Set proper permissions */
+	(void)chmod(name, X509_FILE_PERM);
+	return (0);
 }
 
 /*
@@ -331,13 +326,12 @@ mf_kxlist_setup(struct krb5_inst *ki)
 	/* Retrieve the kx509 credentials, search by service name only */
 	if ((err = krb5_cc_retrieve_cred(ki->ki_ctx, ki->ki_cache,
 	     KRB5_TC_MATCH_SRV_NAMEONLY, &screds, &ki->ki_cred)) !=
-	    KRB5KDC_ERR_NONE)
-	if (err) {
+	    KRB5KDC_ERR_NONE) {
 		mf_log("unable to retrieve kx509 credential (%d)", err);
 		return (HTTP_UNAUTHORIZED);
 	}
 	krb5_free_cred_contents(ki->ki_ctx, &screds);
-	return (OK);
+	return (0);
 }
 
 /*
@@ -372,41 +366,35 @@ mf_get_uid_from_ticket_cache(const char *tkt)
 
 /*
  * Parse the principal and get the uid either from the KDC
- * (XXX if that is even possible!) or just read from /etc/passwd
+ * (XXX if that is even possible) or just read from /etc/passwd
  */
 static int
 mf_user_id_from_principal(const char *principal, char **uid)
 {
 	struct passwd *pw;
-	int i, j, err = 0;
+	int i, j;
 	char *p;
 
 	/* Parse principal. */
-	for (i = 0, j = 0; i < strlen(principal); i++, j++) {
+	for (i = 0, j = 0; i < strlen(principal); i++, j++)
 		if (principal[i] == '@')
 			break;
-	}
 
 	/* slice username */
-	p = mf_dstrslice(principal, 0, j - 1);
-
-	if (p) {
-		pw = getpwnam(p);
-		if (pw == NULL) {
-			mf_log("User not in /etc/passwd");
-			err = HTTP_UNAUTHORIZED;
-		} else {
-			if (asprintf(uid, "%d", pw->pw_uid) == -1) {
-				*uid = NULL;
-				mf_log("asprintf");
-				err = HTTP_INTERNAL_SERVER_ERROR;
-			}
-		}
-	} else {
+	if ((p = mf_dstrslice(principal, 0, j - 1)) == NULL) {
 		mf_log("principal slice error");
-		err = HTTP_INTERNAL_SERVER_ERROR;
+		return (HTTP_INTERNAL_SERVER_ERROR);
 	}
-	return (err);
+	if ((pw = getpwnam(p)) == NULL){
+		mf_log("User not in /etc/passwd");
+		return (HTTP_UNAUTHORIZED);
+	}
+	if (asprintf(uid, "%d", pw->pw_uid) == -1) {
+		*uid = NULL;
+		mf_log("asprintf");
+		return (HTTP_INTERNAL_SERVER_ERROR);
+	}
+	return (0);
 }
 
 static int
@@ -422,9 +410,11 @@ mf_kx509(const char *tkt_cache)
 	/* argv[3] = NULL; */
 	argc = 3;
 
-	if ((err = do_kx509(argc, argv)) != KX509_STATUS_GOOD)
+	if ((err = do_kx509(argc, argv)) != KX509_STATUS_GOOD) {
 		mf_log("kx509 failed (%d)", err);
-	return (err ? HTTP_INTERNAL_SERVER_ERROR : OK);
+		return (HTTP_INTERNAL_SERVER_ERROR);
+	}
+	return (0);
 }
 
 static int
@@ -450,12 +440,11 @@ mf_kinit(struct krb5_inst *ki, struct krb5_prefs *kp)
 		mf_log("get initial credentials failed (%d)", err);
 		return (HTTP_UNAUTHORIZED);
 	}
-
 	ki->ki_init = 1;
 
 	/* Initialize the cache file */
-	err = krb5_cc_initialize(ki->ki_ctx, ki->ki_cache, ki->ki_prin);
-	if (err) {
+	if ((err = krb5_cc_initialize(ki->ki_ctx, ki->ki_cache,
+	     ki->ki_prin)) != KRB5KDC_ERR_NONE) {
 		/*
 		 * In testing, this is thrown with the error -1765328188
 		 * when there are already credentials created and mod_fum
@@ -466,10 +455,12 @@ mf_kinit(struct krb5_inst *ki, struct krb5_prefs *kp)
 	}
 
 	/* Store the credential */
-	err = krb5_cc_store_cred(ki->ki_ctx, ki->ki_cache, &ki->ki_cred);
-	if (err)
+	if ((err = krb5_cc_store_cred(ki->ki_ctx, ki->ki_cache,
+	     &ki->ki_cred)) != KRB5KDC_ERR_NONE) {
 		mf_log("store credentials failed (%d)", err);
-	return (err == KRB5KDC_ERR_NONE ? OK : HTTP_UNAUTHORIZED);
+		return (HTTP_UNAUTHORIZED);
+	}
+	return (0);
 }
 
 /*
@@ -484,19 +475,22 @@ mf_krb5_init(struct krb5_inst *ki, const char *tkt_cache)
 	ki->ki_init = 0;
 
 	/* Initialize application context */
-	if ((err = krb5_init_context(&ki->ki_ctx)) == KRB5KDC_ERR_NONE) {
-		/*
-		 * Don't use the default.  We need to be able to
-		 * write the tkt out with different uid's for each
-		 * individual user.
-		 */
-		/* err = krb5_cc_default(ki->ki_ctx, &ki->cache); */
-		err = krb5_cc_resolve(ki->ki_ctx, tkt_cache, &ki->ki_cache);
-		if (err)
-			mf_log("default cache failed (%d)", err);
-	} else
+	if ((err = krb5_init_context(&ki->ki_ctx)) != KRB5KDC_ERR_NONE) {
 		mf_log("krb5_init_context failed (%d)", err);
-	return (err == KRB5KDC_ERR_NONE ? OK : HTTP_UNAUTHORIZED);
+		return (HTTP_UNAUTHORIZED);
+	}
+	/*
+	 * Don't use the default.  We need to be able to
+	 * write the tkt out with different uid's for each
+	 * individual user.
+	 */
+	/* err = krb5_cc_default(ki->ki_ctx, &ki->cache); */
+	if ((err = krb5_cc_resolve(ki->ki_ctx, tkt_cache,
+	     &ki->ki_cache)) != KRB5KDC_ERR_NONE) {
+		mf_log("default cache failed (%d)", err);
+		return (HTTP_UNAUTHORIZED);
+	}
+	return (0);
 }
 
 static void
@@ -517,24 +511,24 @@ mf_kinit_setup(struct krb5_inst *ki, struct krb5_prefs *kp)
 	krb5_error_code err;
 
 	/*
-	 * Generate a full principal name
-	 * to be used for authentication.
+	 * Generate a full principal name to be used for authentication.
 	 */
-	err = krb5_sname_to_principal(ki->ki_ctx, NULL, NULL, KRB5_NT_SRV_HST,
-	    &ki->ki_prin);
-	if (err) {
+	if ((err = krb5_sname_to_principal(ki->ki_ctx, NULL, NULL,
+	     KRB5_NT_SRV_HST, &ki->ki_prin)) != KRB5KDC_ERR_NONE) {
 		mf_log("create principal failed (%d)", err);
 		return (HTTP_UNAUTHORIZED);
 	}
 
 	/*
-	 * Take the principal name we were given and parse it
-	 * into the appropriate form for authentication protocols
+	 * Take the principal name we were given and parse it into the
+	 * appropriate form for authentication protocols
 	 */
-	err = krb5_parse_name(ki->ki_ctx, kp->kp_prin, &ki->ki_prin);
-	if (err)
+	if ((err = krb5_parse_name(ki->ki_ctx, kp->kp_prin,
+	     &ki->ki_prin)) != KRB5KDC_ERR_NONE) {
 		mf_log("parse_name failed (%d)", err);
-	return (err == KRB5KDC_ERR_NONE ? OK : HTTP_UNAUTHORIZED);
+		return (HTTP_UNAUTHORIZED);
+	}
+	return (0);
 }
 
 static void
@@ -605,34 +599,33 @@ mf_valid_cred(char *principal)
 	}
 	tkt_cache = mf_dstrcat(_PATH_KRB5CERT, uid);
 	free(uid);
-	if (tkt_cache != NULL) {
-		if (mf_krb5_init(&ki, tkt_cache) != OK)
-			return (0);
 
-		/* Grab the kx509 credentials */
-		if (mf_kxlist_setup(&ki) != OK)
-			return (0);
+	if (tkt_cache == NULL)
+		return (0);
+	if (mf_krb5_init(&ki, tkt_cache) != OK)
+		return (0);
 
-		/* Get the expiration time of the cert */
-		end = ki.ki_cred.times.endtime;
-		mf_krb5_free(&ki);
+	/* Grab the kx509 credentials */
+	if (mf_kxlist_setup(&ki) != OK)
+		return (0);
 
-		/* Compare with our time now */
-		if (time(NULL) < end)
-			return (1);
-	}
+	/* Get the expiration time of the cert */
+	end = ki.ki_cred.times.endtime;
+	mf_krb5_free(&ki);
+
+	/* Compare with our time now */
+	if (time(NULL) < end)
+		return (1);
 	return (0);
 }
 
 /*
- * Check if the user/pass is valid
- * (for now just simulate a Kerberos authentication)
+ * Check if the user/pass is valid (for now just simulate a Kerberos
+ * authentication)
  *
- * XXX - There must be a better way to validate the
- * authenticity of the user.
- * XXX - if this is the best way
- * to do this, then i need to try and find a more modular
- * way, because much of this code is similar to kinit.
+ * XXX: there must be a better way to validate the authenticity of the
+ * user.  If this is the best way, it should be more modular, because
+ * much of this code is similar to kinit.
  */
 static int
 mf_valid_user(const char *principal, const char *password)
@@ -685,15 +678,15 @@ mf_dstrcat(const char *s1, const char *s2)
 	char *s;
 
 	len = strlen(s1) + strlen(s2) + 1;
-	s = (char *)(apr_palloc(mf_pool, sizeof(char)*len));
-	if (s == NULL)
+	s = (char *)apr_palloc(mf_pool, sizeof(char)*len);
+	if (s == NULL) {
 		mf_log("malloc failed");
-	else {
-		strncpy(s, s1, strlen(s1));
-		s[strlen(s1)] = '\0';
-		strncat(s, s2, strlen(s2));
-		s[len - 1] = '\0';
+		return (NULL);
 	}
+	(void)strncpy(s, s1, strlen(s1));
+	s[strlen(s1)] = '\0';
+	(void)strncat(s, s2, strlen(s2));
+	s[len - 1] = '\0';
 	return (s);
 }
 
@@ -709,21 +702,22 @@ mf_dstrslice(const char *s, int x, int y)
 	slen = strlen(s);
 	len = y - x + 2;
 
-	if (len) {
-		if ((s2 = apr_palloc(mf_pool, sizeof(char) * len)) ==
-		    NULL) {
-			mf_log("malloc failed");
-			return (NULL);
-		}
-		if (x >= 0 && y >= 0) {
-			if (x < slen && y < slen) {
-				/* ptr increment to start location */
-				s += x;
-				strncpy(s2, s, len - 1);
-				s2[len - 1] = '\0';
-			} else
-				s2 = NULL;
-		}
+	if (!len)
+		/* XXX: fix this shit. */
+		return (s2);
+
+	if ((s2 = apr_palloc(mf_pool, sizeof(char) * len)) == NULL) {
+		mf_log("malloc failed");
+		return (NULL);
+	}
+	if (x >= 0 && y >= 0) {
+		if (x < slen && y < slen) {
+			/* ptr increment to start location */
+			s += x;
+			(void)strncpy(s2, s, len - 1);
+			s2[len - 1] = '\0';
+		} else
+			s2 = NULL;
 	}
 	return (s2);
 }

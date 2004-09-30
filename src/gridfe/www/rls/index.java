aceaa9fd9da822ca6f88e5752e1c009bb47e77a2
yanovich <>
2004-09-30 21:08:42 +0000

    initial checkin

---
+++ src/gridfe/www/rls/index.java
@@ -0,0 +1,21 @@
+/* $Id$ */
+
+package gridfe.www.rls;
+
+import gridfe.*;
+import oof.*;
+
+public class index {
+	public static String main(Page p)
+	  throws Exception {
+		OOF oof = p.getOOF();
+		String s = "";
+
+		s += p.header("Welcome")
+		   + oof.p("Welcome to the portal.")
+		   + p.footer();
+		return (s);
+	}
+};
+
+/* vim: set ts=4: */

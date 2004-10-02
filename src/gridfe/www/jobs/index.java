5b3c6c4fd7ff644b2c5281ba1a39ed70a9393459
yanovich <>
2004-10-02 07:21:57 +0000

    flesh out

--- src/gridfe/www/jobs/index.java
+++ src/gridfe/www/jobs/index.java
@@ -11,8 +11,16 @@ public class index {
 		OOF oof = p.getOOF();
 		String s = "";
 
-		s += p.header("Welcome")
-		   + oof.p("Welcome to the portal.")
+		s += p.header("Job Management")
+		   + oof.p("Please select an action:")
+		   + oof.list(
+				oof.LIST_UN,
+				new Object[] {
+					oof.link("Submit a job", "submit"),
+					oof.link("View status of a job", "status"),
+					oof.link("Collect output from a job", "output")
+				}
+		     )
 		   + p.footer();
 		return (s);
 	}

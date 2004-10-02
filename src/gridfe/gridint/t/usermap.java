b6a106fef891a425e0103a6c60757936d7bbe342
yanovich <>
2004-10-02 22:30:39 +0000

    initial checkin

---
+++ src/gridfe/gridint/t/usermap.java
@@ -0,0 +1,10 @@
+/* $Id$ */
+
+import gridfe.gridint.*;
+
+public class usermap {
+	public static void main(String[] args) {
+		UserMap m = new UserMap();
+		System.out.println(m.kerberosToSystem("yanovich"));
+	}
+};

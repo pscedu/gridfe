302dc57a5bf346d6370dda133f04e4d3d5ddf80e
yanovich <>
2004-10-19 01:39:30 +0000

    simplify this, use PREFIX instead of INSTALL_PATH; add jce-jdk13-120 from bouncy for encoder utils

--- lib/mk/defs.mk
+++ lib/mk/defs.mk
@@ -3,16 +3,17 @@
 MAINMK = ${SYSROOT}/lib/mk/main.mk
 JAVAC = `which javac`
 JAVA = `which java`
-JAVA_INSTALL_PATH = /usr/java/j2sdk1.4.2_05
-COG_INSTALL_PATH = /usr/java/cog-1.2
-SCLASSPATH =						\
-	${SYSROOT}/src:					\
-	/usr/share/java/servlet.jar:			\
-	${JAVA_INSTALL_PATH}/jre/lib/rt.jar:		\
-	${COG_INSTALL_PATH}/lib/cog-jglobus.jar:	\
-	${COG_INSTALL_PATH}/lib/puretls.jar:		\
-	${COG_INSTALL_PATH}/lib/cryptix32.jar:		\
-	${COG_INSTALL_PATH}/lib/cryptix-asn1.jar:	\
+JAVA_PREFIX = /usr/java/j2sdk1.4.2_05
+COG_PREFIX = /usr/java/cog-1.2
+SCLASSPATH =					\
+	${SYSROOT}/src:				\
+	/usr/share/java/servlet.jar:		\
+	${JAVA_PREFIX}/jre/lib/rt.jar:		\
+	${COG_PREFIX}/lib/cog-jglobus.jar:	\
+	${COG_PREFIX}/lib/puretls.jar:		\
+	${COG_PREFIX}/lib/cryptix32.jar:	\
+	${COG_PREFIX}/lib/cryptix-asn1.jar:	\
+	${COG_PREFIX}/lib/jce-jdk13-120.jar:	\
 	.
 CLASSPATH = `echo ${SCLASSPATH} | tr -d '\t\n\ '`
 CFLAGS += -Wall -W -g

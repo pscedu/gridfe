1941284594a570f69944efb073a6709a849d69bb
yanovich <>
2005-05-27 19:25:08 +0000

    typos; make page delegation actually work

--- src/gridfe/www/nodes.java
+++ src/gridfe/www/nodes.java
@@ -3,39 +3,45 @@
 package gridfe.www;
 
 import gridfe.*;
+import java.lang.reflect.*;
+import javax.servlet.http.*;
 import oof.*;
 
 public class nodes {
 	public static String main(Page p)
 	  throws Exception {
+		HttpServletRequest req = p.getRequest();
 		Action[] acts = new Action[] {
-			new Action("add", add),
-			new Action("list", list),
-			new Action("status", status),
-			new Action("remove", remove)
+			new Action("add", "add"),
+			new Action("list", "list"),
+			new Action("status", "status"),
+			new Action("remove", "remove")
 		};
 
-		for (int i = 0; i < acts.length; i++)
-			if (acts[i].getName().equals(act))
-				return (acts[i].getMethod().invoke(null, new Object[] { p }));
+		String act = req.getParameter("action");
+		if (act != null)
+			for (int i = 0; i < acts.length; i++)
+				if (acts[i].getName().equals(act))
+					return ((String)Class.forName("nodes").getMethod(acts[i].getMethod(),
+					  new Class[] { Page.class }).invoke("S", new Object[] { p }));
 		return (status(p));
 	}
 
 	public static String add(Page p)
 	  throws Exception {
+		HttpServletRequest req = p.getRequest();
 		OOF oof = p.getOOF();
 		String s = "", emsg;
-		HttpServletRequest req = p.getRequest();
 
 		if (req.getParameter("submitted") != null) {
 			String host = req.getParameter("host");
 
 			if (host == null)
 				emsg = "Invalid host: " + p.escapeHTML(host);
-			} else {
+			else {
 //				add(host);
 				s += p.header("Host Added")
-				   + p.p("The host " + p.escapeHTML(host) +
+				   + oof.p("The host " + p.escapeHTML(host) +
 						" has been successfully added.");
 				return (s);
 			}
@@ -58,7 +64,7 @@ public class nodes {
 		String s = "";
 
 
-		s += p.header("Node Availibility")
+		s += p.header("Node Availibility");
 		return (s);
 	}
 
@@ -68,7 +74,7 @@ public class nodes {
 		String s = "";
 
 
-		s += p.header("Node Availibility")
+		s += p.header("Node Availibility");
 		return (s);
 	}
 
@@ -78,9 +84,9 @@ public class nodes {
 
 class Action {
 	private String name;
-	private Method meth;
+	private String m;
 
-	public Action(String name, Method m) {
+	public Action(String name, String m) {
 		this.name = name;
 		this.m = m;
 	}
@@ -89,7 +95,7 @@ class Action {
 		return (this.name);
 	}
 
-	public Method getName() {
+	public String getMethod() {
 		return (this.m);
 	}
 };

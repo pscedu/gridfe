18cff0754e4ccd2e8a15396eadb91ef3abbf067e
yanovich <>
2005-08-19 20:45:36 +0000

    info for idiots; layout tweaks; error message when no jobs

--- src/gridfe/www/jobs/status.java
+++ src/gridfe/www/jobs/status.java
@@ -11,25 +11,37 @@ public class status {
 	  throws Exception {
 		OOF oof = p.getOOF();
 		String s = "";
+		int i;
 
 		GridInt gi = p.getGridInt();
 		JobList list = gi.getJobList();
 		GridJob j;
 
 		s += p.header("Job Status")
+		  +  oof.p("This page contains the status information for any jobs that " +
+		  		"jobs that you have submitted.  For completed jobs, you may follow " +
+				"the link to the " + oof.link("Job Output", p.buildURL("/jobs/output")) +
+				" page to retrieve any output that the job may have generated.")
 		  +  oof.table_start(new Object[] {
 			  "class", p.CCTBL,
 			  "border", "0",
 			  "cellspacing", "0",
 			  "cellpadding", "0" })
 		  +		oof.table_row(new Object[][] {
-					new Object[] { "class", p.CCDESC, "value", "ID" },
-					new Object[] { "class", p.CCDESC, "value", "Name" },
-					new Object[] { "class", p.CCDESC, "value", "Host" },
-//					new Object[] { "class", p.CCDESC, "value", "Remote" },
-					new Object[] { "class", p.CCDESC, "value", "Status" }
+		  			new Object[] {
+						"class", p.CCHDR,
+						"colspan", "5",
+						"value", "Job Status"
+					}
+		  		})
+		  +		oof.table_row(new Object[][] {
+					new Object[] { "class", p.CCSUBHDR, "value", "ID" },
+					new Object[] { "class", p.CCSUBHDR, "value", "Name" },
+					new Object[] { "class", p.CCSUBHDR, "value", "Host" },
+//					new Object[] { "class", p.CCSUBHDR, "value", "Remote" },
+					new Object[] { "class", p.CCSUBHDR, "value", "Status" }
 				});
-		for (int i = 0; i < list.size(); i++) {
+		for (i = 0; i < list.size(); i++) {
 			j = list.get(i);
 			String c = p.genClass();
 			s += oof.table_row(new Object [][] {
@@ -40,6 +52,15 @@ public class status {
 				new Object[] { "class", c, "value", j.getStatusAsString() }
 			});
 		}
+		if (i == 0)
+			s += oof.table_row(new Object[][] {
+				new Object[] {
+					"class", "data1",
+					"colspan", "5",
+					"value", "No jobs currently in queue.  " +
+						oof.link("Submit a new job.", p.buildURL("/jobs/submit"))
+				}
+			});
 		s += oof.table_end()
 		  +  p.footer();
 		return (s);

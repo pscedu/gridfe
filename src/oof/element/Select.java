b0ab302f03f8b99a92dcdff6d20dd64b9149b361
yanovich <>
2005-08-22 14:06:43 +0000

    initial checkin

---
+++ src/oof/element/Select.java
@@ -0,0 +1,12 @@
+/* $Id$ */
+package oof.element;
+
+import oof.*;
+import oof.element.*;
+
+public class Select extends _Element {
+	public Select(OOF oof, Object[] attrs, Object[] os)
+	    throws OOFBadElementFormException {
+		super(oof, attrs, os);
+	}
+};
---
+++ src/oof/element/Textarea.java
@@ -0,0 +1,12 @@
+/* $Id$ */
+package oof.element;
+
+import oof.*;
+import oof.element.*;
+
+public class Textarea extends _Element {
+	public Textarea(OOF oof, Object[] attrs, Object[] os)
+	    throws OOFBadElementFormException {
+		super(oof, attrs, os);
+	}
+};

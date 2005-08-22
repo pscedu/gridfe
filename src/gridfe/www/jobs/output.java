d4acfaa3013b4026c7fe1ca9862d4942c3554db3
rbudden <>
2005-08-22 18:47:09 +0000

    output page test

--- src/gridfe/www/jobs/output.java
+++ src/gridfe/www/jobs/output.java
@@ -4,15 +4,43 @@ package gridfe.www.jobs;
 
 import gridfe.*;
 import oof.*;
+import gridfe.gridint.*;
 
 public class output {
-	public static String main(Page page)
+	public static String main(Page p)
 	  throws Exception {
-		OOF oof = page.getOOF();
+		OOF oof = p.getOOF();
+		GridInt gi;
+		JobList jl;
+		int size, i;
+		String s = "";
 
-		return	page.header("Job Output") +
-				oof.p("Job output test page.") +
-				page.footer();
+		gi = p.getGridInt();
+		jl = gi.getJobList();
+
+		/* XXX:
+		** retrieve size to have a progress metere
+		** showing how much of the file has been transfered
+		*/
+//		size = gi.gass.getSize();
+		size = jl.size();
+
+		s += p.header("Job Output") +
+			 oof.p("Please select a job:");
+
+		/* Loop and show jobs */
+		Object[] jobs = new Object[size];
+		for(i = 0; i < size; i++)
+		{
+//			jobs[i] = oof.link(jl.getJob(i).getName(), "/jobs/retrieve");
+			s += oof.p("/jobs/retrieve");
+		}
+
+//		s += oof.list(oof.LIST_UN, jobs);
+
+		s += p.footer();
+
+		return s;
 	}
 };
 

f3ba719956cee9cc4f877fb41318831ea7e4df6c
yanovich <>
2005-08-22 16:39:49 +0000

    initial checkin

---
+++ src/oof/element/Option.java
@@ -0,0 +1,12 @@
+/* $Id$ */
+package oof.element;
+
+import oof.*;
+import oof.element.*;
+
+public class Option extends _Element {
+	public Option(OOF oof, Object[] attrs, Object[] os)
+	    throws OOFBadElementFormException {
+		super(oof, attrs, os);
+	}
+};

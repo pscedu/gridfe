0a7dd52a8ef7543bbf74b61ef6594ad3cb9e69ee
yanovich <>
2005-08-22 13:28:24 +0000

    spacing

--- src/oof/OOFBadElementFormException.java
+++ src/oof/OOFBadElementFormException.java
@@ -4,10 +4,12 @@ package oof;
 
 public class OOFBadElementFormException extends OOFException {
 	private String tag;
+
 	public OOFBadElementFormException(String tag) {
 		super();
 		this.tag = tag;
 	}
+
 	public String getMessage() {
 		return this.tag + ": required attributes missing";
 	}

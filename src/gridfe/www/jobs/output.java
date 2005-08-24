793f003b50ab8db21b45219242f7bd7d582f0d02
yanovich <>
2005-08-24 23:06:35 +0000

    initial flesh, this shit now works

--- src/gridfe/www/jobs/output.java
+++ src/gridfe/www/jobs/output.java
@@ -3,44 +3,45 @@
 package gridfe.www.jobs;
 
 import gridfe.*;
-import oof.*;
 import gridfe.gridint.*;
+import javax.servlet.http.*;
+import oof.*;
 
 public class output {
 	public static String main(Page p)
 	  throws Exception {
-		OOF oof = p.getOOF();
-		GridInt gi;
-		JobList jl;
-		int size, i;
 		String s = "";
+		OOF oof = p.getOOF();
+		GridInt gi = p.getGridInt();
+		JobList jl = gi.getJobList();
+		String s_qid;
+		HttpServletRequest req = p.getRequest();
 
-		gi = p.getGridInt();
-		jl = gi.getJobList();
+		if ((s_qid = req.getParameter("qid")) != null &&
+		  s_qid.matches("^\\d+$")) {
+			/* This should never throw an exception since we just checked it. */
+			GridJob j = jl.get(Integer.parseInt(s_qid));
+			if (j != null) {
+				String fn_stdout = (j.dir == null ? "" : j.dir) + j.stdout;
+				gi.startRetrieve(j, fn_stdout, 28000, 28255);
+				int size = (int)gi.getGass().getSize();
+/*
+				if (size > )
+					error
+*/
+				String data = gi.retrieve(size, 0);
+				gi.stopRetrieve();
 
-		/* XXX:
-		** retrieve size to have a progress metere
-		** showing how much of the file has been transfered
-		*/
-//		size = gi.gass.getSize();
-		size = jl.size();
-
-		s += p.header("Job Output") +
-			 oof.p("Please select a job:");
-
-		/* Loop and show jobs */
-		Object[] jobs = new Object[size];
-		for(i = 0; i < size; i++)
-		{
-//			jobs[i] = oof.link(jl.getJob(i).getName(), "/jobs/retrieve");
-			s += oof.p("/jobs/retrieve");
+				s += p.header("Job Output")
+				  +  oof.pre(p.escapeHTML(data))
+				  +  p.footer();
+				return (s);
+			}
 		}
-
-//		s += oof.list(oof.LIST_UN, jobs);
-
-		s += p.footer();
-
-		return s;
+		s += p.header("Error")
+		  +  oof.p("Sorry, the system could not find the requested job.")
+		  +  p.footer();
+		return (s);
 	}
 };
 

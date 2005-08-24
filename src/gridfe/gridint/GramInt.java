fbb6c3935bba527a0bec4f0ae9287aeebedfa67f
yanovich <>
2005-08-24 21:46:23 +0000

    jnf

--- src/gridfe/gridint/GramInt.java
+++ src/gridfe/gridint/GramInt.java
@@ -29,14 +29,14 @@ public class GramInt {
 	public void jobSubmit(GridJob j)
 	    throws GramException, GSSException {
 		/* Make sure the host is there */
-		System.out.println("Trying to Ping Host");
+		System.out.println("Trying to ping host");
 		Gram.ping(this.gss, this.host);
 
 		/* Create and process Job */
 		this.job = this.createJob(j.toString());
 		System.out.println("Trying GramJob.request()");
 		this.job.request(this.host, true);
-		System.out.println("GramJob.request() finished!");
+		System.out.println("GramJob.request() finished");
 	}
 
 	/* Used internally to by this.gramRequest and GridJob.revive */
@@ -49,22 +49,22 @@ public class GramInt {
 	public int getStatus()
 	    throws GSSException {
 		/*
-		** TODO: Find a good way to tell if the
-		** job has a status of FAIL or DONE after
-		** the jobmanager has termianted and the
-		** GramException (below) is thrown.
-		*/
+		 * TODO: Find a good way to tell if the
+		 * job has a status of FAIL or DONE after
+		 * the jobmanager has termianted and the
+		 * GramException (below) is thrown.
+		 */
 		int status = -2;
 
 		try {
 			Gram.jobStatus(this.job);
 			status = this.job.getStatus();
-		} catch(GramException e) {
+		} catch (GramException e) {
 			/*
-			** Job manager cannont be contacted. Therefore the job
-			** is done. However, we do not know if the job finished
-			** normally or terminated. -1 can stand for DONE/FAIL.
-			*/
+			 * Job manager cannont be contacted. Therefore the job
+			 * is done. However, we do not know if the job finished
+			 * normally or terminated. -1 can stand for DONE/FAIL.
+			 */
 			if (e.getErrorCode() ==
 			    GramException.ERROR_CONTACTING_JOB_MANAGER)
 				status = -1;
@@ -80,7 +80,7 @@ public class GramInt {
 		try {
 			Gram.jobStatus(this.job);
 			status = this.job.getStatusAsString();
-		} catch(GramException e) {
+		} catch (GramException e) {
 			/* See above .getStatus() for information on this. */
 			if (e.getErrorCode() ==
 			    GramException.ERROR_CONTACTING_JOB_MANAGER)

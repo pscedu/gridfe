f2b1dc6e2b9c51223d40120959ba54a0be25f83d
yanovich <>
2005-08-30 19:25:56 +0000

    add some more initial stuff to get it working; mysql does not support semicolons in comments...

--- share/sql/create.sql
+++ share/sql/create.sql
@@ -1,28 +1,34 @@
 /* $Id$ */
 
+DROP TABLE IF EXISTS gridints;
 CREATE TABLE gridints (
-	uid	VARCHAR(32),
+	uid	VARCHAR(32)	NOT NULL,
 	gi	TINYBLOB,
 
-	PRIMARY_KEY(uid)
+	PRIMARY KEY(uid)
 );
 
 /*
+
 CREATE TABLE jobs (
 	uid	VARCHAR(32),
 	label	VARCHAR(50),
-);
+)
+
 */
 
+DROP TABLE IF EXISTS hosts;
 CREATE TABLE hosts (
-	uid	VARCHAR(32),
+	uid	VARCHAR(32)	NOT NULL,
 	host	VARCHAR(255),
 
-	PRIMARY_KEY(uid),
+	PRIMARY KEY(uid),
 	KEY(host)
 );
 
 /*
+
 CREATE TABLE x509_certs (
-);
+)
+
 */

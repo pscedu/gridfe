3c3174dd3d93e9c00be3a193f0b1515dd127dc54
yanovich <>
2006-04-21 16:13:10 +0000

    new cog stuff: commons-logging.jar, cog-jglobus moved into gt-specific subdir

--- lib/mk/defs.mk
+++ lib/mk/defs.mk
@@ -3,15 +3,16 @@
 MAINMK = ${SYSROOT}/lib/mk/main.mk
 JAVAC = ${JAVA_PREFIX}/bin/javac
 JAVA = ${JAVA_PREFIX}/bin/java
-SCLASSPATH =					\
-	${SYSROOT}/src:				\
-	/usr/share/java/servlet.jar:		\
-	${JAVA_PREFIX}/jre/lib/rt.jar:		\
-	${COG_PREFIX}/lib/cog-jglobus.jar:	\
-	${COG_PREFIX}/lib/puretls.jar:		\
-	${COG_PREFIX}/lib/cryptix32.jar:	\
-	${COG_PREFIX}/lib/cryptix-asn1.jar:	\
-	${COG_PREFIX}/lib/jce-jdk13-125.jar:	\
+SCLASSPATH =						\
+	${SYSROOT}/src:					\
+	/usr/share/java/servlet.jar:			\
+	${JAVA_PREFIX}/jre/lib/rt.jar:			\
+	${COG_PREFIX}/lib/gt4_0_0/cog-jglobus.jar:	\
+	${COG_PREFIX}/lib/puretls.jar:			\
+	${COG_PREFIX}/lib/commons-logging.jar:		\
+	${COG_PREFIX}/lib/cryptix32.jar:		\
+	${COG_PREFIX}/lib/cryptix-asn1.jar:		\
+	${COG_PREFIX}/lib/jce-jdk13-125.jar:		\
 	.
 CLASSPATH = `echo ${SCLASSPATH} | tr -d '\t\n\ '`
 CFLAGS += -Wall -W -g

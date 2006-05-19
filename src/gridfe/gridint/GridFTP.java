f12e83af4a9cbfe42ea4b86b28046b31884495d5
rbudden <>
2006-05-19 16:39:55 +0000

    initial checkin; urlCopy for simple GridFTP file transfer

---
+++ src/gridfe/gridint/GridFTP.java
@@ -0,0 +1,44 @@
+/* $Id$ */
+
+package gridfe.gridftp;
+
+import java.io.*;
+import java.net.*;
+import org.ietf.jgss.*;
+import org.globus.io.urlcopy.*;
+import org.globus.util.*;
+
+public class GridFtp {
+	
+	private GSSCredential gss;
+
+	public GridFtp(GSSCredential gss) {
+		this.gss = gss;	
+	}
+
+	/*
+	** Use GridFTP to copy a file.
+	** host in the form of: gridfe.psc.edu
+	** file in the form of: /home/rbudden/foo (assumes absolute)
+	*/
+	public void urlCopy(String shost, String dhost, String sfile, String dfile)
+		throws MalformedURLException, UrlCopyException {
+
+		/* Use gsiftp to transfer */
+		UrlCopy url = new UrlCopy();
+		
+		/* Set the credentials to use */
+		url.setCredentials(this.gss);
+
+		/* Add an extra "/" after the host for absolute, (bug in CoG) */
+		GlobusURL su = new GlobusURL("gsiftp://"+shost+"/"+sfile);
+		GlobusURL du = new GlobusURL("gsiftp://"+dhost+"/"+dfile);
+
+		/* Always use Third Party */
+		url.setUseThirdPartyCopy(true);
+
+		url.setSourceUrl(su);
+		url.setDestinationUrl(du);
+		url.copy();
+	}
+}

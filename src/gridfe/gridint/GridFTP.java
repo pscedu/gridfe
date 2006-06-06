ab406c8974471f0491cf8efaec2328e83df7b401
yanovich <>
2006-06-06 19:25:28 +0000

    cleanup a little

--- src/gridfe/gridint/GridFTP.java
+++ src/gridfe/gridint/GridFTP.java
@@ -10,39 +10,40 @@ import org.globus.io.urlcopy.*;
 import org.globus.util.*;
 import org.globus.ftp.*;
 import org.globus.ftp.exception.*;
+import org.globus.gram.*;
+import org.ietf.jgss.*;
 import gridfe.gridint.*;
 
 public class GridFTP extends GridFTPClient {
-	
 	private GSSCredential gss;
 
 	public GridFTP(GSSCredential gss, String host, int port)
-		throws IOException, ServerException, ClientException {
+	  throws IOException, ServerException, ClientException {
 		super(host, port);
-		this.gss = gss;	
+		this.gss = gss;
 		this.authenticate(this.gss);
 	}
 
-	public Vector ls() 
-		throws IOException, ServerException, ClientException {
+	public Vector ls()
+	  throws IOException, ServerException, ClientException {
 		return this.list(null, null);
 	}
 
-	public Vector ls(String path) 
-		throws IOException, ServerException, ClientException {
+	public Vector ls(String path)
+	  throws IOException, ServerException, ClientException {
 		return this.nlist(path);
 	}
 
 	/* Wrap the GridFTP FileInfo code to use our GridFile class */
 	public Vector gls()
-		throws IOException, ServerException, ClientException {
+	  throws IOException, ServerException, ClientException {
 		Vector v = this.list(null, null);
 		return GridFTP.fi2GridFile(v);
 	}
 
 	public Vector gls(String path)
-		throws IOException, ServerException, ClientException {
-		Vector v = this.list(path);
+	  throws IOException, ServerException, ClientException {
+		Vector v = this.nlist(path);
 		return GridFTP.fi2GridFile(v);
 	}
 
@@ -72,7 +73,7 @@ public class GridFTP extends GridFTPClient {
 
 		for(int i = 0; i < v.size(); i++)
 			gv.add(new GridFile((FileInfo)(v.get(i))));
-			
+
 		return gv;
 	}
 
@@ -81,18 +82,18 @@ public class GridFTP extends GridFTPClient {
 	** host in the form of: gridfe.psc.edu
 	** file in the form of: /home/rbudden/foo (assumes absolute)
 	*/
-	public static void urlCopy(GSSCredential gss, String shost, String dhost, String sfile, String dfile)
-		throws MalformedURLException, UrlCopyException {
-
+	public static void urlCopy(GSSCredential gss, String shost,
+	  String dhost, String sfile, String dfile)
+	  throws MalformedURLException, UrlCopyException {
 		/* Use gsiftp to transfer */
 		UrlCopy url = new UrlCopy();
-		
+
 		/* Set the credentials to use */
 		url.setCredentials(gss);
 
 		/* Add an extra "/" after the host for absolute, (bug in CoG) */
-		GlobusURL su = new GlobusURL("gsiftp://"+shost+"/"+sfile);
-		GlobusURL du = new GlobusURL("gsiftp://"+dhost+"/"+dfile);
+		GlobusURL su = new GlobusURL("gsiftp://" + shost + "/" + sfile);
+		GlobusURL du = new GlobusURL("gsiftp://" + dhost + "/" + dfile);
 
 		/* Always use Third Party */
 		url.setUseThirdPartyCopy(true);

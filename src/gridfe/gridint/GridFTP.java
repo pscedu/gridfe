192caa9f343af347be73c1113b5266dc625b8cc2
yanovich <>
2006-07-05 14:41:34 +0000

    set globus system property for firewall port range; comment out unnecessary code (remove?); test Mlsx entry to list style (didnt work, remove?); add gsiScp job for future file transfers

--- src/gridfe/gridint/GridFTP.java
+++ src/gridfe/gridint/GridFTP.java
@@ -13,59 +13,74 @@ import org.globus.ftp.exception.*;
 import org.globus.gram.*;
 import org.ietf.jgss.*;
 import gridfe.gridint.*;
+import org.globus.ftp.MlsxEntry;
 
 public class GridFTP extends GridFTPClient {
+	private static final String _PATH_GSISCP = "/usr/psc/bin/gsiscp";
+
 	private GSSCredential gss;
 
 	public GridFTP(GSSCredential gss, String host, int port)
 	  throws IOException, ServerException, ClientException {
 		super(host, port);
+
+//		java.lang.System.setProperty("org.globus.tcp.port.range", "28000,28255");
+		java.lang.System.setProperty("org.globus.tcp.port.range", "50000,51000");
+
 		this.gss = gss;
 		this.authenticate(this.gss);
+
+		System.out.println("Authenticated to host "+host+":"+port);
+
+		/* Ugly test to chose port range */
+		/*
+		try {
+			this.list();
+		} catch (Exception e) {
+			throw new IOException("no route to host");
+		}
+		*/
 	}
 
+/*
 	public Vector ls()
 	  throws IOException, ServerException, ClientException {
 		return this.list(null, null);
 	}
+*/
 
+/*
 	public Vector ls(String path)
 	  throws IOException, ServerException, ClientException {
 		return this.nlist(path);
 	}
+*/
 
 	/* Wrap the GridFTP FileInfo code to use our GridFile class */
 	public Vector gls()
 	  throws IOException, ServerException, ClientException {
-		Vector v = this.list(null, null);
+		System.out.println("Trying GridFTP.gls()");
+//		Vector v = this.list(null, null);
+		Vector v = this.list();
+		System.out.println("Successful GridFTP.gls()");
 		return GridFTP.fi2GridFile(v);
 	}
 
+/*
 	public Vector gls(String path)
 	  throws IOException, ServerException, ClientException {
 		Vector v = this.nlist(path);
 		return GridFTP.fi2GridFile(v);
 	}
-
-/*
-	public void mv(String s, String d) {
-		this.rename(s, d);
-	}
-
-	public void rm() {
-	}
-
-	public String pwd() {
-		return this.getCurrentDir();
-	}
-	public void cd() {
-		this.changeDir();
-	}
-
-	public void mkdir(String dir) {
-		this.makeDir(dir);
-	}
 */
+
+	public Vector test()
+	  throws IOException, ServerException, ClientException {
+		System.out.println("Trying GridFTP.mlsd()");
+		Vector v = this.mlsd();
+		System.out.println("Successful GridFTP.mlsd()");
+		return GridFTP.mlsx2GridFile(v);
+	}
 
 	/* Convert FileInfo Vectors to GridFile Vectors */
 	public static Vector fi2GridFile(Vector v) {
@@ -73,6 +88,24 @@ public class GridFTP extends GridFTPClient {
 
 		for(int i = 0; i < v.size(); i++)
 			gv.add(new GridFile((FileInfo)(v.get(i))));
+
+		return gv;
+	}
+
+	/* Convert MlsxEntry Vectors to GridFile Vectors */
+	public static Vector mlsx2GridFile(Vector v) {
+		Vector gv = new Vector(v.size());
+
+		for(int i = 0; i < v.size(); i++) {
+			GridFile f = new GridFile();
+			MlsxEntry e = (MlsxEntry)(v.get(i));
+			f.name = e.getFileName();
+			f.date = "1";
+			f.time = "2";
+			f.perm = "3";
+			f.size = 4;
+			gv.add(f);
+		}
 
 		return gv;
 	}
@@ -101,5 +134,22 @@ public class GridFTP extends GridFTPClient {
 		url.setSourceUrl(su);
 		url.setDestinationUrl(du);
 		url.copy();
+	}
+
+	/* Submit a job to a machine to perform a gsiscp between it and another host */
+	public static void gsiScp(GridInt gi, String manager, String shost,
+		String dhost, String sfile, String dfile)
+		throws GSSException, GramException {
+
+		GridJob j = new GridJob(manager);
+
+		/* gsiscp gridfe:/home/rbudden/foo ben:/home/rbudden/bar */
+		HashMap m = j.getMap();
+		m.put("executable", _PATH_GSISCP);
+		m.put("arguments", new String[] {
+		  shost + sfile, dhost + dfile } );
+
+		j.init(gi.getGSS().getGSSCredential());
+		j.run();
 	}
 }

af4e24c26d09855a3e41622e91ee2aa596bda0b3
yanovich <>
2006-07-05 14:09:58 +0000

    kill splitString

--- src/jasp/GetPWEnt.java
+++ src/jasp/GetPWEnt.java
@@ -6,63 +6,35 @@ import java.io.*;
 import java.util.*;
 
 public class GetPWEnt {
-
-//	struct passwd {
-//	char    *pw_name;       /* user name */
-//	char    *pw_passwd;     /* encrypted password */
-//	uid_t   pw_uid;         /* user uid */
-//	gid_t   pw_gid;         /* user gid */
-//	time_t  pw_change;      /* password change time */
-//	char    *pw_class;      /* user access class */
-//	char    *pw_gecos;      /* Honeywell login info */
-//	char    *pw_dir;        /* home directory */
-//	char    *pw_shell;      /* default shell */
-//	time_t  pw_expire;      /* account expiration */
-//	int     pw_fields;      /* internal: fields filled in */
-//	};
-
 	public String pw_name;
-//	public String pw_password;
 	public int pw_uid;
 	public int pw_gid;
-//	public time_t pw_change;
-//	public String pw_class;
-//	public String pw_gecos;
 	public String pw_dir;
 	public String pw_shell;
-//	public time_t  pw_expire;
 	public int pw_fields;
 
 	/* It is sad that there is no way to do this in J2SE. */
 	public GetPWEnt(String username) {
-
 		try {
-			/*
-			 * An equivalent to getpwent() might be
-			 * better here...
-			 */
 			String line;
 			String[] fields;
 			File f = new File("/etc/passwd");
 			BufferedReader r = new BufferedReader(new
 			    FileReader(f));
 			while ((line = r.readLine()) != null) {
-				fields = splitString(line, ":");
+				fields = line.split(":");
 				if (fields.length > 0 &&
 				    fields[0].equals(username)) {
-					this.uid = Integer.valueOf(fields[2]).intValue();
-					this.gid = Integer.valueOf(fields[3]).intValue();
+					this.pw_uid = Integer.valueOf(fields[2]).intValue();
+					this.pw_gid = Integer.valueOf(fields[3]).intValue();
 					this.pw_dir = fields[5];
 					this.pw_shell = fields[6];
 					break;
 				}
 			}
 			r.close();
-
 			this.pw_name = username;
-
 		} catch (Exception e) {
-			return (-1);
 		}
 	}
 };
--- src/jasp/t/suite.java
+++ src/jasp/t/suite.java
@@ -6,8 +6,6 @@ public class suite {
 	public static void main(String[] args) {
 		System.out.println("User ID: " + new Integer(
 			BasicServices.getUserID()));
-		String[] pieces = BasicServices.splitString("foo/bar/baz", "/");
-		for (int i = 0; i < pieces.length; i++)
-			System.out.println(i + ": " + pieces[i]);
+		/* XXX add getpwent test */
 	}
 };

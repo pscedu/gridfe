6446661abdc7f53649a9ed12db02086fcc0c5fc4
yanovich <>
2006-08-30 18:51:59 +0000

    preliminary rss support

--- src/gridfe/www/jobs/status.java
+++ src/gridfe/www/jobs/status.java
@@ -4,16 +4,23 @@ package gridfe.www.jobs;
 
 import gridfe.*;
 import gridfe.gridint.*;
+import java.text.*;
 import java.util.*;
+import javax.servlet.http.*;
 import oof.*;
 import org.globus.gram.*;
 
 public class status {
 	public static String main(Page p)
 	  throws Exception {
+		HttpServletRequest req = p.getRequest();
 		OOF oof = p.getOOF();
 		String s = "";
 		int i;
+
+		String out = req.getParameter("out");
+		if (out != null && out.equals("rss"))
+			return (rss(p));
 
 		GridInt gi = p.getGridInt();
 		GridJob j;
@@ -115,6 +122,95 @@ public class status {
 		  +  oof.table_end()
 		  +  oof.form_end()
 		  +  p.footer();
+		return (s);
+	}
+
+	public static String rss(Page p) {
+		GridInt gi = p.getGridInt();
+		String s = "";
+
+		/* XXX use p.buildPath */
+		String logo = "http://gridfe.psc.edu/gridfe/img/gridfe2sm.png";
+
+		/* 2006-08-18T17:31:43+00:00 */
+		Format fmt1 = new SimpleDateFormat("yyyy-MM-dd");
+		Format fmt2 = new SimpleDateFormat("HH:mm:ss");
+		Format fmt3 = new SimpleDateFormat("Z");
+		Date now = new Date();
+		String date = fmt1.format(now) + "T" + fmt2.format(now);
+		String tz = fmt3.format(now);
+
+		if (tz.startsWith("-"))
+			date += "-";
+		else
+			date += "+";
+
+		date += tz.substring(tz.length() - 4, tz.length() - 2) +
+		  ":" + tz.substring(tz.length() - 2);
+
+		s += "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>"
+		  +	 "<rdf:RDF "
+		  +	 " xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\" "
+		  +	 " xmlns=\"http://purl.org/rss/1.0/\" "
+		  +	 " xmlns:dc=\"http://purl.org/dc/elements/1.1/\" "
+		  +	 " xmlns:syn=\"http://purl.org/rss/1.0/modules/syndication/\">"
+		  +  	"<channel rdf:about=\"http://gridfe.psc.edu/\">"
+		  +  		"<title>GridFE Jobs</title>"
+		  +  		"<link>http://gridfe.psc.edu/gridfe/gridfe/jobs</link>"
+		  +  		"<description>GridFE - grid front end</description>"
+		  +  		"<dc:language>en-us</dc:language>"
+		  +				"<dc:rights>"
+		  +					"Copyright (c) 2006 Pittsburgh Supercomputing Center"
+		  +				"</dc:rights>"
+		  +  		"<dc:date>" + date + "</dc:date>"
+//		  +  		"<dc:publisher>PSC</dc:publisher>"
+		  +  		"<syn:updatePeriod>hourly</syn:updatePeriod>"
+		  +  		"<syn:updateFrequency>6</syn:updateFrequency>"
+		  +  		"<syn:updateBase>1970-01-01T00:00+00:00</syn:updateBase>"
+		  +  		"<image rdf:resource=\"" + logo + "\" />";
+
+		String seqs = "", items = "";
+		List list = gi.getJobList().getList();
+		for (int i = 0; i < list.size(); i++) {
+			GridJob j = (GridJob)list.get(i);
+
+			String url = p.buildURL("/jobs/output?qid=" + j.getQID());
+			String stat = "unknown";
+
+			try {
+				stat = j.getStatusAsString();
+			} catch (Exception e) {
+			}
+
+			seqs += "<rdf:li rdf:resource=\"" + url + "\" />";
+			items +=	"<item rdf:about=\"" + url + "\">"
+				  +			"<title>"
+				  +				p.escapeHTML(j.getName() + ": " + stat)
+				  +			"</title>"
+				  + 		"<link>" + url + "</link>"
+				  +			"<description>"
+				  + 			"name: " + j.getHost() + "; \n"
+				  + 			"host: " + j.getHost() + "; \n"
+				  + 			"status: " + stat
+				  + 		"</description>"
+//				  + 		"<dc:date>2006-08-18T04:01:00+00:00</dc:date>"
+				  +		"</item>";
+		}
+
+		s +=		"<items>"
+		  +				"<rdf:Seq>"
+		  +					seqs
+		  +				"</rdf:Seq>"
+		  +			"</items>"
+		  +			"</channel>"
+		  +			"<image rdf:about=\"" + logo + "\">"
+		  +				"<title>GridFE Jobs</title>"
+		  +					"<url>" + logo + "</url>"
+		  +				"<link>http://gridfe.psc.edu/gridfe/gridfe/</link>"
+		  +			"</image>"
+		  +			items
+		  +		"</rdf:RDF>";
+		p.getResponse().setContentType("text/xml");
 		return (s);
 	}
 };
